ee55c5b8bed420ec263fc85a52ba2a5af1316342
lexoyo <a.hoyau@im-paris.fr>
2013-09-21 15:23:35 +0200

    compil

--- server/cloud-explorer/unifile/unifile-config.js
+++ server/cloud-explorer/unifile/unifile-config.js
@@ -64,7 +64,7 @@ exports.gdrive =
 	auth_uri : "https://accounts.google.com/o/oauth2/auth",
 	client_secret : "8H5OatS9dhhnKlDL5pnVZ0Kr",
 	token_uri : "https://accounts.google.com/o/oauth2/token",
-	client_email : "1092494939602-js3q389sjqn9hql42g1o9mktpb4fcan3@developer.gserviceaccount.com","redirect_uris":["http://unifile.silexlabs.org/v1.0/gdrive/auth_callback/"],
+	client_email : "1092494939602-js3q389sjqn9hql42g1o9mktpb4fcan3@developer.gserviceaccount.com","redirect_uris":["http://unifile.silexlabs.org/api/v1.0/gdrive/auth_callback/"],
 	client_x509_cert_url : "https://www.googleapis.com/robot/v1/metadata/x509/1092494939602-js3q389sjqn9hql42g1o9mktpb4fcan3@developer.gserviceaccount.com",
 	client_id : "1092494939602-js3q389sjqn9hql42g1o9mktpb4fcan3.apps.googleusercontent.com",
 	auth_provider_x509_cert_url : "https://www.googleapis.com/oauth2/v1/certs",
