f688293d2f7516706160f005d5d21647524f6b89
yanhick <yannick.dominguez@gmail.com>
2013-11-24 00:38:32 +0100

    implementing google fonts

---
+++ packages/silex-lib/src/js/model/config.js
@@ -0,0 +1,24 @@
+
+//////////////////////////////////////////////////
+// Silex, live web creation
+// http://projects.silexlabs.org/?/silex/
+//
+// Copyright (c) 2012 Silex Labs
+// http://www.silexlabs.org/
+//
+// Silex is available under the GPL license
+// http://www.silexlabs.org/silex/silex-licensing/
+//////////////////////////////////////////////////
+
+/**
+ * @fileoverview Silex config
+ */
+
+
+goog.provide('silex.model.Config');
+
+silex.model.Config.fonts = {
+    'test' : {
+        href : 'test url'
+    }
+};
