a9dc6da9cbef495e7c89ae15e1d9c8fc1496804c
TheCause <thecause@free.fr>
2013-11-24 02:35:48 +0100

    Added fonts to text box

--- src/js/model/config.js
+++ src/js/model/config.js
@@ -22,22 +22,97 @@ goog.provide('silex.model.Config');
  */
 silex.model.Config.fonts = {
 
+
+    'Roboto Condensed' : {
+        //the url to load the font file	
+        href : 'http://fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,300,700',
+        //the value for the CSS font-family value
+        value : 'Roboto Condensed'
+    },
     'Roboto' : {
 
-        //the url to load the font file
         href : 'http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic',
 
-        //the value for the CSS font-family value
         value : 'Roboto'
     },
+    'Days One' : {
 
+        href : 'http://fonts.googleapis.com/css?family=Days+One',
+
+        value : 'Days One'
+    },
+    'Sintony' : {
+
+        href : 'http://fonts.googleapis.com/css?family=Sintony:400,700',
+
+        value : 'Sintony'
+    },
+    'Junge' : {
+
+        href : 'http://fonts.googleapis.com/css?family=Junge',
+
+        value : 'Junge'
+    },
+    'Istok Web' : {
+
+        href : 'http://fonts.googleapis.com/css?family=Istok+Web:400,700,400italic,700italic',
+
+        value : 'Istok Web'
+    },
+    'Oswald' : {
+
+        href : 'http://fonts.googleapis.com/css?family=Oswald:400,300,700',
+
+        value : 'Oswald'
+    },
+    'Cantata' : {
+
+        href : 'http://fonts.googleapis.com/css?family=Cantata+One',
+
+        value : 'Cantata'
+    },
+    'Oranienbaum' : {
+
+        href : 'http://fonts.googleapis.com/css?family=Oranienbaum',
+
+        value : 'Oranienbaum'
+    },
+    'Londrina Solid' : {
+
+        href : 'http://fonts.googleapis.com/css?family=Londrina+Solid',
+
+        value : 'Londrina Solid'
+    },
+    'Noticia Text' : {
+
+        href : 'http://fonts.googleapis.com/css?family=Noticia+Text:400,400italic,700,700italic',
+
+        value : 'Noticia Text'
+    },
     'Codystar' : {
 
-        href : 'http://fonts.googleapis.com/css?family=Codystar:400',
+        href : 'http://fonts.googleapis.com/css?family=Codystar:300,400',
 
         value : 'Codystar'
     },
+    'Titillium Web' : {
 
+        href : 'http://fonts.googleapis.com/css?family=Titillium+Web:400,200,200italic,300,300italic,400italic,600,600italic,700,700italic,900',
+
+        value : 'Titillium Web'
+    },
+    'Sarina' : {
+
+        href : 'http://fonts.googleapis.com/css?family=Sarina',
+
+        value : 'Sarina'
+    },
+    'Bree Serif' : {
+
+        href : 'http://fonts.googleapis.com/css?family=Bree+Serif',
+
+        value : 'Bree Serif'
+    },
     'Arial Black' : {
         value : 'Arial Black, Gadget, sans-serif'
     },
@@ -60,5 +135,5 @@ silex.model.Config.fonts = {
 
     'Tahoma' : {
         value : 'Tahoma, Geneva, sans-serif'
-    },
+    }
 };
