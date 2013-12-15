12a819455ae717d393ff07832f3843ddc0849987
lexoyo <a.hoyau@im-paris.fr>
2013-12-15 18:28:03 +0100

    params to deploy on heroku

--- packages/silex-lib/Gruntfile.js
+++ packages/silex-lib/Gruntfile.js
@@ -35,14 +35,14 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-html');
   grunt.loadNpmTasks('grunt-closure-linter');
   grunt.loadNpmTasks('grunt-simple-mocha');
   //grunt.loadNpmTasks('grunt-contrib-jade');
 
   grunt.task.renameTask('watch', 'doWatch')
 
-  grunt.registerTask('deploy', ['concat', 'less:production', 'less:development', 'closureBuilder:debug', 'closureBuilder:release', 'append-sourcemapping']);
+  grunt.registerTask('deploy', ['debugDeploy', 'releaseDeploy']);
+  grunt.registerTask('releaseDeploy', ['concat', 'less:production', 'closureBuilder:release']);
   grunt.registerTask('debugDeploy', ['concat', 'less:development', 'closureBuilder:debug', 'append-sourcemapping']);
   grunt.registerTask('check', ['htmllint', 'csslint:lax', 'closureLint']);
   grunt.registerTask('test', ['deploy', 'simplemocha']);
@@ -67,9 +67,14 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask('install', function() {
+    try{
       var fs = require('fs');
       grunt.file.copy('build/pre-commit', '.git/hooks/pre-commit');
       fs.chmodSync('.git/hooks/pre-commit', '755');
+    }
+    catch(e){
+      console.log('not able to add precommit hook.');
+    }
   });
   grunt.task.run('install');
 
@@ -128,12 +133,12 @@ module.exports = function(grunt) {
           cleancss: true
         }
         , files: {
-          "dist/client/css/admin.min.css": "src/css/.temp"
+          "dist/client/css/admin.css": "src/css/.temp"
         }
       }
       , production: {
         files: {
-          "dist/client/css/admin.css": "src/css/.temp"
+          "dist/client/css/admin.min.css": "src/css/.temp"
         }
       }
     }
