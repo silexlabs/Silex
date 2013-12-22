0c978891f190845c3fff0e9820c56fc964738f46
lexoyo <a.hoyau@im-paris.fr>
2013-12-22 13:05:57 +0100

    debug mode, and tests to prevent commit with debug mode

--- packages/silex-lib/test/functional/menu.js
+++ packages/silex-lib/test/functional/menu.js
@@ -43,7 +43,7 @@ before(function(done) {
     this.timeout(30000);
     helper.startSelenium(function (_) {
         // open silex
-        helper.driver.get('http://localhost:6805/silex/debug.html').then(function () {
+        helper.driver.get('http://localhost:6805/silex/index.html').then(function () {
             done();
         });
     });
