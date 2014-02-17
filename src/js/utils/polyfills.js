cc5f545433ec6a4058ce199cee9a67d5e19638be
lexoyo <a.hoyau@silexlabs.org>
2014-02-17 23:51:05 +0100

    added console as a polyfill for IE

---
+++ src/js/utils/polyfills.js
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////////
+// Silex, live web creation
+// http://projects.silexlabs.org/?/silex/
+//
+// Copyright (c) 2012 Silex Labs
+// http://www.silexlabs.org/
+//
+// Silex is available under the GPL license
+// http://www.silexlabs.org/silex/silex-licensing/
+//////////////////////////////////////////////////
+
+/**
+ * @fileoverview add polyfills to the browser if needed
+ *
+ */
+
+
+goog.provide('silex.utils.Polyfills');
+
+
+/**
+ * @fileoverview add polyfills to the browser if needed
+ *
+ */
+silex.utils.Polyfills.init = function () {
+  // console object, do nothing
+  console = console || {
+    log: function () {}
+    , warn: function () {}
+    , error: function () {}
+  };
+};
