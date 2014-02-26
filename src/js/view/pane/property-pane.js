b58be7fc09f249ac9ddfa814b2493eb17c64714c
lexoyo <a.hoyau@silexlabs.org>
2014-02-26 21:01:03 +0100

    fixed firefox have undefined in the position and size input fields

--- src/js/view/pane/property-pane.js
+++ src/js/view/pane/property-pane.js
@@ -177,8 +177,8 @@ silex.view.pane.PropertyPane.prototype.redraw = function() {
   var bb = silex.utils.Dom.getBoundingBox(elements);
 
   // display position and size
-  this.topInput.value = bb.top;
-  this.leftInput.value = bb.left;
-  this.widthInput.value = bb.width;
-  this.heightInput.value = bb.height;
+  this.topInput.value = bb.top || '';
+  this.leftInput.value = bb.left || '';
+  this.widthInput.value = bb.width || '';
+  this.heightInput.value = bb.height || '';
 };
