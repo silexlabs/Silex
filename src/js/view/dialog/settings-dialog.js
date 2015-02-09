d584cdea08dcbb5d2cf90f6d60e0fa1d61f0c865
lexoyo <a.hoyau@silexlabs.org>
2015-02-09 14:47:42 +0100

    added social network meta data in settings

--- src/js/view/dialog/settings-dialog.js
+++ src/js/view/dialog/settings-dialog.js
@@ -47,44 +47,21 @@ silex.view.dialog.SettingsDialog = function(element, controller) {
   var leftPane = goog.dom.getElementByClass('left-pane');
   goog.events.listen(
       leftPane, goog.events.EventType.CLICK, this.onNavClick, false, this);
-  // title input field
-  var inputTitle = goog.dom.getElementByClass('input-title');
-  goog.events.listen(
-      inputTitle, goog.ui.Component.EventType.CHANGE,
-      function() {
-        this.controller.settingsDialogController.setTitle(inputTitle.value);
-      }, false, this);
-  // description input field
-  var inputDescription = goog.dom.getElementByClass('input-description');
-  goog.events.listen(
-      inputDescription, goog.ui.Component.EventType.CHANGE,
-      function() {
-        this.controller.settingsDialogController.setDescription(inputDescription.value);
-      }, false, this);
-  // favicon path browse button
-  var faviconPathBtn = goog.dom.getElementByClass('browse-favicon-path', this.element);
-  goog.events.listen(faviconPathBtn, goog.events.EventType.CLICK, function() {
-    this.controller.settingsDialogController.browseFaviconPath();
-  }, false, this);
-  // favicon path input field
-  var inputFaviconPath = goog.dom.getElementByClass('input-favicon-path');
-  goog.events.listen(
-      inputFaviconPath, goog.ui.Component.EventType.CHANGE,
-      function() {
-        this.controller.settingsDialogController.setFaviconPath(inputFaviconPath.value);
-      }, false, this);
-  // publication path browse button
-  var publicationPathBtn = goog.dom.getElementByClass('browse-publication-path', this.element);
-  goog.events.listen(publicationPathBtn, goog.events.EventType.CLICK, function() {
-    this.controller.settingsDialogController.browsePublishPath();
-  }, false, this);
-  // publication path input field
-  var inputPublicationPath = goog.dom.getElementByClass('input-publication-path');
-  goog.events.listen(
-      inputPublicationPath, goog.ui.Component.EventType.CHANGE,
-      function() {
-        this.controller.settingsDialogController.setPublicationPath(inputPublicationPath.value);
-      }, false, this);
+
+  // input text fields
+  this.bindTextField('.general-pane .input-title', (v) => this.controller.settingsDialogController.setTitle(v));
+  this.bindTextField('.social-pane .input-title', (v) => this.controller.settingsDialogController.setTitleSocial(v));
+  this.bindTextField('.general-pane .input-description', (v) => this.controller.settingsDialogController.setDescription(v));
+  this.bindTextField('.social-pane .input-description', (v) => this.controller.settingsDialogController.setDescriptionSocial(v));
+  this.bindTextField('.social-pane .input-twitter', (v) => this.controller.settingsDialogController.setTwitterSocial(v));
+  this.bindTextField('.general-pane .input-favicon-path', (v) => this.controller.settingsDialogController.setFaviconPath(v));
+  this.bindTextField('.social-pane .input-image-path', (v) => this.controller.settingsDialogController.setThumbnailSocialPath(v));
+  this.bindTextField('.publish-pane .input-publication-path', (v) => this.controller.settingsDialogController.setPublicationPath(v));
+
+  // image path browse button
+  this.bindBrowseButton('.general-pane .browse-favicon-path', () => this.controller.settingsDialogController.browseFaviconPath())
+  this.bindBrowseButton('.social-pane .browse-image-path', () => this.controller.settingsDialogController.browseThumbnailSocialPath())
+  this.bindBrowseButton('.publish-pane .browse-publication-path', () => this.controller.settingsDialogController.browsePublishPath())
 };
 
 // inherit from silex.view.dialog.DialogBase
@@ -96,6 +73,7 @@ goog.inherits(silex.view.dialog.SettingsDialog, silex.view.dialog.DialogBase);
  */
 silex.view.dialog.SettingsDialog.PANE_CSS_CLASSES = [
   'general-pane',
+  'social-pane',
   'publish-pane'
 ];
 
@@ -129,13 +107,49 @@ silex.view.dialog.SettingsDialog.prototype.onNavClick = function(e) {
 
 
 /**
+ * binds an input element with a callback
+ * @param {string} cssSelector
+ * @param {function(string)} cbk
+ */
+silex.view.dialog.SettingsDialog.prototype.bindTextField = function(cssSelector, cbk) {
+  // title input field
+  var input = this.element.querySelector(cssSelector);
+  if(!input) {
+    throw new Error('Settings panel error: could not find the element to bind.');
+  }
+  goog.events.listen(
+      input, goog.ui.Component.EventType.CHANGE,
+      function() {
+        cbk(input.value);
+      }, false, this);
+};
+
+
+/**
+ * binds a button element with a callback
+ * @param {string} cssSelector
+ * @param {function()} cbk
+ */
+silex.view.dialog.SettingsDialog.prototype.bindBrowseButton = function(cssSelector, cbk) {
+  // title input field
+  var btn = this.element.querySelector(cssSelector);
+  if(!btn) {
+    throw new Error('Settings panel error: could not find the element to bind.');
+  }
+  goog.events.listen(btn, goog.events.EventType.CLICK, function() {
+    cbk();
+  }, false, this);
+};
+
+
+/**
  * set the value to the input element
  * @see silex.model.Head
- * @param {string} cssClass   the input css class
+ * @param {string} cssSelector
  * @param {?string=} opt_value
  */
-silex.view.dialog.SettingsDialog.prototype.setInputValue = function(cssClass, opt_value) {
-  var input = goog.dom.getElementByClass(cssClass);
+silex.view.dialog.SettingsDialog.prototype.setInputValue = function(cssSelector, opt_value) {
+  var input = this.element.querySelector(cssSelector);
   if (opt_value) {
     input.value = opt_value;
   }
@@ -146,22 +160,32 @@ silex.view.dialog.SettingsDialog.prototype.setInputValue = function(cssClass, op
 
 
 /**
- * set the pubication path to display
- * @see silex.model.Head
- * @param {?string=} opt_path   the publication path
- */
-silex.view.dialog.SettingsDialog.prototype.setPublicationPath = function(opt_path) {
-  this.setInputValue('input-publication-path', opt_path);
-}
-
-
-/**
  * set the favicon path to display
  * @see silex.model.Head
  * @param {?string=} opt_path
  */
 silex.view.dialog.SettingsDialog.prototype.setFaviconPath = function(opt_path) {
-  this.setInputValue('input-favicon-path', opt_path);
+  this.setInputValue('.general-pane .input-favicon-path', opt_path);
+}
+
+
+/**
+ * set the social image path to display
+ * @see silex.model.Head
+ * @param {?string=} opt_path
+ */
+silex.view.dialog.SettingsDialog.prototype.setThumbnailSocialPath = function(opt_path) {
+  this.setInputValue('.social-pane .input-image-path', opt_path);
+}
+
+
+/**
+ * set the pubication path to display
+ * @see silex.model.Head
+ * @param {?string=} opt_path   the publication path
+ */
+silex.view.dialog.SettingsDialog.prototype.setPublicationPath = function(opt_path) {
+  this.setInputValue('.publish-pane .input-publication-path', opt_path);
 }
 
 
@@ -171,7 +195,7 @@ silex.view.dialog.SettingsDialog.prototype.setFaviconPath = function(opt_path) {
  * @param {?string=} opt_title   the site title
  */
 silex.view.dialog.SettingsDialog.prototype.setTitle = function(opt_title) {
-  this.setInputValue('input-title', opt_title);
+  this.setInputValue('.general-pane .input-title', opt_title);
 };
 
 
@@ -181,7 +205,37 @@ silex.view.dialog.SettingsDialog.prototype.setTitle = function(opt_title) {
  * @param {?string=} opt_description   the site description
  */
 silex.view.dialog.SettingsDialog.prototype.setDescription = function(opt_description) {
-  this.setInputValue('input-description', opt_description);
+  this.setInputValue('.general-pane .input-description', opt_description);
+};
+
+
+/**
+ * set the site title to display
+ * @see silex.model.Head
+ * @param {?string=} opt_title   the site title
+ */
+silex.view.dialog.SettingsDialog.prototype.setTitleSocial = function(opt_title) {
+  this.setInputValue('.social-pane .input-title', opt_title);
+};
+
+
+/**
+ * set the site description tag
+ * @see silex.model.Head
+ * @param {?string=} opt_description   the site description
+ */
+silex.view.dialog.SettingsDialog.prototype.setDescriptionSocial = function(opt_description) {
+  this.setInputValue('.social-pane .input-description', opt_description);
+};
+
+
+/**
+ * set the owner twitter account
+ * @see silex.model.Head
+ * @param {?string=} opt_twitter
+ */
+silex.view.dialog.SettingsDialog.prototype.setTwitterSocial = function(opt_twitter) {
+  this.setInputValue('.social-pane .input-twitter', opt_twitter);
 };
 
 
