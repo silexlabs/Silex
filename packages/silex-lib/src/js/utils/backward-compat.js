da0651e193344eab77510217c1775809d0e2a0c4
lexoyo <a.hoyau@silexlabs.org>
2015-03-24 17:22:35 +0100

    display an alert when updating to the latest version

--- packages/silex-lib/src/js/utils/backward-compat.js
+++ packages/silex-lib/src/js/utils/backward-compat.js
@@ -59,9 +59,12 @@ silex.utils.BackwardCompat.process = function(doc, model, cbk) {
       return parseInt(str, 10) || 0;
     });
 
+  // warn the user
   if (silex.utils.BackwardCompat.amIObsolete(version, silex.utils.BackwardCompat.LATEST_VERSION)) {
     silex.utils.Notification.alert('This website has been saved with a newer version of Silex. Continue at your own risks.', function() {});
-
+  }
+  if (silex.utils.BackwardCompat.hasToUpdate(version, silex.utils.BackwardCompat.LATEST_VERSION)) {
+    silex.utils.Notification.alert('This website has been updated with the latest version of Silex.<br><br>Before you save it, please check that everything is fine. Saving it with another name could be a good idea too (menu file > save as).', function() {});
   }
 
   // update static.silex.me
