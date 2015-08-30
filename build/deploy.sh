51505f673e09a6b556c4d56b48af6657bde83423
lexoyo <a.hoyau@silexlabs.org>
2015-08-30 12:16:58 +0200

    fixed deployment on production server

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
+# This script is used for CI, when the deployment is not straightforward as with heroku.
+# It is called by the CI deployment server, e.g. from `circle.yml`.
+# See this article about deployment http://the.webapp.cat/2015/07/Deploy-to-Gandi-Simple-Hosting.html
+#
+# Params:
+# * $1 is the ssh host to deploy to, e.g. abcd@efg.com
+# * $2 is either "prod" or "preprod", which is the domain name where to call the "reload route"
+#
+# Useful environement variables:
+# * `SILEX_RELOAD_ROUTE`: the path to call to reboot the nodejs server
 
 ls -al dist/client/js/
 zip -r silex.zip dist/ node_modules/ static/
 echo 'put silex.zip' | sftp -o "StrictHostKeyChecking no" "$1":vhosts/default
-echo "wget http://preprod.silex.me/$SILEX_RELOAD_ROUTE"
-wget "http://preprod.silex.me/$SILEX_RELOAD_ROUTE"
+echo "wget $2/$SILEX_RELOAD_ROUTE"
+wget "$2/$SILEX_RELOAD_ROUTE"
