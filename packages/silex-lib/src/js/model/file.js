b9b9d41e2c8d78cf8e66ab5f5973fb56f0f1b26d
lexoyo <a.hoyau@silexlabs.org>
2015-09-03 19:41:38 +0200

    more robust backward compat

--- packages/silex-lib/src/js/model/file.js
+++ packages/silex-lib/src/js/model/file.js
@@ -214,8 +214,8 @@ silex.model.File.prototype.contentChanged = function(opt_cbk) {
 silex.model.File.prototype.onContentLoaded = function(opt_cbk) {
   // if the pageable plugin is not created yet, come back later
   // handle retrocompatibility issues
-  silex.utils.BackwardCompat.process(this.contentDocument_, this.model, () => {
-    // check the integrity and store silex style sheet which holds silex elements styles
+  silex.utils.BackwardCompat.process(this.contentDocument_, this.model, (hasUpgraded) => {
+   // check the integrity and store silex style sheet which holds silex elements styles
     this.model.property.initSilexStyleTag(this.contentDocument_);
     this.model.property.setCurrentSilexStyleSheet(this.model.property.getSilexStyleSheet(this.contentDocument_));
     // select the body
@@ -229,7 +229,15 @@ silex.model.File.prototype.onContentLoaded = function(opt_cbk) {
     this.model.head.updateFromDom();
     // restore event listeners
     this.view.stage.initEvents(this.contentWindow_);
-    // refresh the view
+    // if upgraded, relaod everything
+    if(hasUpgraded) {
+      // wait for the BC to complete and the dom to update
+      setTimeout(() => {
+        this.setHtml(this.getHtml(), opt_cbk);
+      }, 200);
+      return;
+    }
+     // refresh the view
     //var page = this.model.page.getCurrentPage();
     //this.model.page.setCurrentPage(page);
     // notify the caller
