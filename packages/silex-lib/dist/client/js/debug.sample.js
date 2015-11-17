d6d716ef069354521cab9f9d395b3770a230a918
lexoyo <a.hoyau@silexlabs.org>
2015-11-17 20:05:02 +0100

    fixed debug script

--- packages/silex-lib/dist/client/js/debug.sample.js
+++ packages/silex-lib/dist/client/js/debug.sample.js
@@ -1,7 +1,7 @@
 // wait some time so that the black website loads
 setTimeout(function(){
   // insert elements
-  window.controller.insertMenuController.addElement('text');
-  window.controller.insertMenuController.addElement('html');
-  window.controller.insertMenuController.addElement('container');
+  window.controller.$insertMenuController$.addElement('text');
+  window.controller.$insertMenuController$.addElement('html');
+  window.controller.$insertMenuController$.addElement('container');
 }, 2000);
