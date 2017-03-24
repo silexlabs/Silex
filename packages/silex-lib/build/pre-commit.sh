74da90657e7f91e06a817b18bdf6c475e23d6af2
Alex Hoyau <a.hoyau@silexlabs.org>
2017-03-24 16:00:07 +0100

    clean up package.json, removed unused tests, put back linter on the server code

--- packages/silex-lib/build/pre-commit.sh
+++ packages/silex-lib/build/pre-commit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 function lintit () {
-  OUTPUT=$(npm run lint:js)
+  OUTPUT=$(npm run lint:js && npm run lint:server)
   if [[ "$OUTPUT" == *"problems"* ]]; then
     echo "ERROR: Check eslint hints, run \`npm run lint:js\`"
     exit 1 # reject
