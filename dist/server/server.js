0fb3ba0d7eaddbf3c9096671cf4bc6c1ed760300
JbIPS <jeanbaptiste.richardet@gmail.com>
2017-04-21 15:52:12 +0200

    Splits server file

--- dist/server/server.js
+++ dist/server/server.js
@@ -44,76 +44,7 @@ app.use('/', cookieParser());
 
 // Start Silex as an Electron app
 if(process.env.SILEX_ELECTRON) {
-  const {app, BrowserWindow, protocol} = require('electron');
-  // Keep a global reference of the window object, if you don't, the window will
-  // be closed automatically when the JavaScript object is garbage collected.
-  let win;
-
-  function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({
-      width: 1200,
-      height: 800,
-      icon: Path.join(__dirname, '..', 'client', 'assets', 'logo-silex.png'),
-      titleBarStyle: 'hidden',
-      webPreferences: {
-        nodeIntegration: false,
-        // Needed by CE callback
-        // Could be remove with next version
-        sandbox: true
-      },
-
-    });
-
-    // and load the index.html of the app.
-    const url = 'http://localhost:' + (process.env.PORT || 6805) + '/';
-    console.log('Sarting app on ' + url);
-    win.loadURL(url);
-
-    // Open the DevTools.
-    win.webContents.openDevTools();
-
-    // Emitted when the window is closed.
-    win.on('closed', () => {
-      // Dereference the window object, usually you would store windows
-      // in an array if your app supports multi windows, this is the time
-      // when you should delete the corresponding element.
-      win = null
-    });
-  }
-
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
-  app.on('ready', createWindow);
-
-  // Quit when all windows are closed.
-  app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-      app.quit()
-    }
-  });
-
-  app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-      createWindow()
-    }
-  });
-
-  //protocol.registerStandardSchemes(['silex']);
-  app.on('ready', () => {
-    protocol.registerFileProtocol('silex', (request, callback) => {
-      // TODO Remove this hack
-      const pathname = url.parse(request.url).pathname.replace('home/home', 'home');
-      callback({path: pathname});
-    }, (error) => {
-      if (error) console.error('Failed to register protocol')
-    });
-  });
+  require(Path.join(__dirname, 'silex_electron'));
 }
 // get silex config
 var silexConfig = unifile.defaultConfig;
@@ -185,7 +116,7 @@ else {
 }
 
 // change www root
-silexConfig.www.ROOT = process.env.SILEX_ELECTRON ? Os.homedir() : Path.join(__dirname, '..', '..', 'dist', 'client');
+silexConfig.www.ROOT = Os.homedir();
 
 // add static folders
 silexConfig.staticFolders.push(
