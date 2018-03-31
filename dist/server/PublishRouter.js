05bbae8cf4e7dfd5bde5ed56f8828f9f1bab5ee1
Alex Hoyau <a.hoyau@silexlabs.org>
2018-03-31 15:21:13 +0200

    changes for @JbIPS code review (thx)

--- dist/server/PublishRouter.js
+++ dist/server/PublishRouter.js
@@ -1,6 +1,5 @@
 const PublishJob = require('./PublishJob.js');
 const express = require('express');
-const uuid = require('uuid');
 const bodyParser = require('body-parser');
 
 module.exports = function(port, rootUrl, unifile) {
@@ -10,8 +9,6 @@ module.exports = function(port, rootUrl, unifile) {
   // **
   // publication tasks
   router.post('/tasks/:task', bodyParser.json(), (req, res, next) => {
-    // init the session (shouldn't it be done by express??
-    req.session.sessionID = req.session.sessionID || uuid.v4();
     switch(req.params.task) {
       case 'publish':
         PublishJob.create(req.session.sessionID, req.body, unifile, req.session, req.cookies, req.protocol + '://' + req.get('host'));
