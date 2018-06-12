4a28651ae85fcf79954493efdb2b749e525cfb09
Alex Hoyau <a.hoyau@silexlabs.org>
2018-06-12 19:26:02 +0200

    renamed new-website-dialog to Dashboard, moved blank templates in the same list as community templates, fix icons in the recent files list, updated version of silex-blank-templates repo

--- src/js/app.js
+++ src/js/app.js
@@ -71,7 +71,7 @@ goog.require('silex.view.dialog.FileExplorer');
 goog.require('silex.view.dialog.HtmlEditor');
 goog.require('silex.view.dialog.JsEditor');
 goog.require('silex.view.dialog.SettingsDialog');
-goog.require('silex.view.dialog.NewWebsiteDialog');
+goog.require('silex.view.dialog.Dashboard');
 goog.require('silex.view.dialog.TextEditor');
 
 goog.require('silex.view.ModalDialog');
@@ -151,7 +151,7 @@ class App {
     this.view.contextMenu.buildUi();
     this.view.breadCrumbs.buildUi();
     this.view.pageTool.buildUi();
-    this.view.newWebsiteDialog.buildUi();
+    this.view.dashboard.buildUi();
     this.view.propertyTool.buildUi();
 
 
@@ -265,10 +265,10 @@ class App {
     /** @type {SettingsDialog} */
     var settingsDialog = new SettingsDialog(settingsDialogElement, this.model, this.controller);
 
-    // NewWebsiteDialog
-    var newWebsiteDialogElement = /** @type {!Element} */ (goog.dom.getElementByClass('silex-newwebsite-dialog'));
-    /** @type {NewWebsiteDialog} */
-    var newWebsiteDialog = new NewWebsiteDialog(newWebsiteDialogElement, this.model, this.controller);
+    // Dashboard
+    var dashboardElement = /** @type {!Element} */ (goog.dom.getElementByClass('silex-dashboard'));
+    /** @type {Dashboard} */
+    var dashboard = new Dashboard(dashboardElement, this.model, this.controller);
 
     // FileExplorer
     var fileExplorerElement = /** @type {!Element} */ (document.getElementById('silex-file-explorer'));
@@ -308,7 +308,7 @@ class App {
         textEditor,
         fileExplorer,
         settingsDialog,
-        newWebsiteDialog,
+        dashboard,
         propSplitter,
         workspace
     );
