bd5fe69be5a71fc11a2e289f756bf23ddf913934
Alex Hoyau <a.hoyau@silexlabs.org>
2018-08-30 16:30:42 +0200

    back to gandi deployment method

--- packages/silex-lib/build/deploy.sh
+++ packages/silex-lib/build/deploy.sh
@@ -3,13 +3,18 @@
 # It is called by the CI deployment server, e.g. from `circle.yml` or `.travis.yml`.
 # See this article about deployment http://the.webapp.cat/2015/07/Deploy-to-Gandi-Simple-Hosting.html
 #
-# Params:
+# Params
 # * $1 is the ssh host of the server, e.g. abcd@efg.com
-# * $2 is the path on the server where we want the zip file to be deployed
-# * $3 is the password for ssh
+# * $2 is the URL of the server where we deploy, e.g. https://editor.silex.me
+# * $3 is the password for sftp
 #
 # Useful environement variables:
 # * `SILEX_RELOAD_ROUTE`: the path to call to reboot the nodejs server
+#
+# Params for "normal deployment", so not gandi, **commented bellow, do not use**:
+# * $1 is the ssh host of the server, e.g. abcd@efg.com
+# * $2 is the path on the server where we want the zip file to be deployed
+# * $3 is the password for ssh/sftp
 
 # ls a generated folder to check that compilation did go well
 ls -al dist/client/js/
@@ -21,11 +26,16 @@ zip -r silex.zip dist/server dist/client node_modules/ static/ package.json
 echo 'put silex.zip' | sshpass -p "$3" sftp -o "StrictHostKeyChecking no" "$1:$2"
 
 # unzip in a tmp folder
-sshpass -p "$3" ssh "$1" "cd $2 && rm -rf tmp-deploy ; unzip silex.zip -d tmp-deploy"
+# sshpass -p "$3" ssh "$1" "cd $2 && rm -rf tmp-deploy ; unzip silex.zip -d tmp-deploy"
 
 # rename
-sshpass -p "$3" ssh "$1" "cd $2 && rm silex.zip ; rm -rf old-Silex ; mv Silex old-Silex ; mv tmp-deploy Silex"
+# sshpass -p "$3" ssh "$1" "cd $2 && rm silex.zip ; rm -rf old-Silex ; mv Silex old-Silex ; mv tmp-deploy Silex"
 
 # reboot the server
-sshpass -p "$3" ssh "$1" "shutdown -r now"
+# sshpass -p "$3" ssh "$1" "shutdown -r now"
+
+
+# this is for deployments on gandi simple hosting, since it has no ssh
+# call a route on silex which will unzip, mv, and  restart
+wget "$2/$SILEX_RELOAD_ROUTE"
 
