16ac082ef0e3aeca801d4efccc2b35c6865acafc
Alex Hoyau <a.hoyau@silexlabs.org>
2018-09-12 12:03:04 +0200

    added github pages hosting provider

---
+++ dist/server/HostingGhPages.js
@@ -0,0 +1,126 @@
+//////////////////////////////////////////////////
+// Silex, live web creation
+// http://projects.silexlabs.org/?/silex/
+//
+// Copyright (c) 2012 Silex Labs
+// http://www.silexlabs.org/
+//
+// Silex is available under the GPL license
+// http://www.silexlabs.org/silex/silex-licensing/
+//////////////////////////////////////////////////
+
+const express = require('express');
+
+module.exports = function(unifile) {
+  // create a router to return
+  const router = express.Router();
+  // add my options to the list of hosting providers
+  router.get('/hosting/', (req, res, next) => {
+    req.session = req.session || {};
+    req.session.unifile = req.session.unifile || {};
+    res.locals = res.locals || [];
+    res.locals.providers = res.locals.providers || [];
+    const infos = unifile.getInfos(req.session.unifile, 'github');
+    const options = {
+      name: 'ghpages',
+      displayName: 'Github Pages',
+      isLoggedIn: infos.isLoggedIn,
+      username: infos.username,
+      authorizeUrl: '/ce/github/authorize',
+      dashboardUrl: 'https://www.github.com',
+      pleaseCreateAVhost: 'create an empty repository.',
+      vhostsUrl: '/hosting/ghpages/vhost',
+      buyDomainUrl: 'https://www.gandi.net',
+      skipVhostSelection: false,
+    };
+    res.locals.providers.push(options);
+    next();
+  });
+  // add my callbacks to the router
+  router.get('/hosting/ghpages/vhost', (req, res, next) => {
+    unifile.readdir(req.session.unifile, 'github', '/')
+      .then(result => {
+        res.json(result
+          .sort((a, b) => new Date(b.modified) - new Date(a.modified))
+          //.slice(0, 1) // max number of repos
+          .map(file => {
+            return {
+              name: file.name,
+              domainUrl: `/hosting/ghpages/vhost/${ file.name }`,
+              skipDomainSelection: false,
+              publicationPath: {
+                absPath: `/ce/github/get/${ file.name }/gh-pages`,
+                isDir: true,
+                mime: 'application/octet-stream',
+                name: 'gh-pages',
+                folder: file.name,
+                path: `${ file.name }/gh-pages`,
+                service: 'github',
+                url: `http://localhost:6805/ce/github/get/${ file.name }/gh-pages`,
+              }
+            };
+          })
+        );
+      })
+      .catch(err => {
+        res.status(400).send({
+          message: `Error from hosting provider "${ req.params.id }": ${ err.message }`,
+          err: err,
+        });
+      });
+  });
+  router.get('/hosting/ghpages/vhost/:name', (req, res, next) => {
+    unifile.readFile(req.session.unifile, 'github', `/${ req.params.name }/gh-pages/CNAME`)
+      .then(result => {
+        res.json({
+          domain: result.toString().replace(/\n/g, ''),
+          https: true,
+        });
+      })
+      .catch(err => {
+        res.json({
+          domain: `${ req.session.unifile.github.account.login }.github.io/${ req.params.name }`,
+          https: true,
+          msg: err,
+        });
+      });
+  });
+  router.post('/hosting/ghpages/vhost/:name', (req, res, next) => {
+    unifile.writeFile(req.session.unifile, 'github', `/${ req.params.name }/gh-pages/CNAME`, req.body.domain)
+      .then(result => {
+        res.json({
+          domain: req.body.domain,
+          https: true,
+        });
+      })
+      .catch(err => {
+        res.status(400).send({
+          message: `Error when trying to attach a domain to "${ req.params.id }". Error details: ${ err.message }`,
+          err: err,
+        });
+      });
+  });
+  router.delete('/hosting/ghpages/vhost/:name', (req, res, next) => {
+    function genericDomain() {
+      res.json({
+        domain: `${ req.session.unifile.github.account.login }.github.io/${ req.params.name }`,
+        https: true,
+      });
+    }
+    unifile.unlink(req.session.unifile, 'github', `/${ req.params.name }/gh-pages/CNAME`, req.body.domain)
+      .then(result => genericDomain())
+      .catch(err => {
+        if(err.code === 'ENOENT') {
+          // there was no CNAME file
+          genericDomain();
+        }
+        else {
+          res.status(400).send({
+            message: `Error when trying to remove domain from "${ req.params.name }". Error details: ${ err.message }`,
+            err: err,
+          });
+        }
+      });
+  });
+  return router;
+};
--- dist/server/PublishRouter.js
+++ dist/server/PublishRouter.js
@@ -1,9 +1,26 @@
+//////////////////////////////////////////////////
+// Silex, live web creation
+// http://projects.silexlabs.org/?/silex/
+//
+// Copyright (c) 2012 Silex Labs
+// http://www.silexlabs.org/
+//
+// Silex is available under the GPL license
+// http://www.silexlabs.org/silex/silex-licensing/
+//////////////////////////////////////////////////
+
 const PublishJob = require('./PublishJob.js');
+const HostingGhPages = require('./HostingGhPages.js');
 const express = require('express');
 
-module.exports = function({ port, rootUrl }, unifile) {
+module.exports = function({ port, rootUrl, enableGithubPages, skipProviderSelection }, unifile) {
 
   const router = express.Router();
+
+  if(enableGithubPages) {
+    const p = require('./HostingGhPages.js');
+    router.use(new p(unifile));
+  }
 
   // **
   // publication tasks
@@ -44,116 +61,10 @@ module.exports = function({ port, rootUrl }, unifile) {
     }
   });
   router.get('/hosting/', (req, res, next) => {
-    const infos = unifile.getInfos(req.session.unifile, 'github')
-      const providers = [
-        {
-          name: 'ghpages',
-          displayName: 'Github Pages',
-          isLoggedIn: infos.isLoggedIn,
-          username: infos.username,
-          authorizeUrl: '/ce/github/authorize',
-          dashboardUrl: 'https://www.github.com',
-          pleaseCreateAVhost: 'create an empty repository.',
-          vhostsUrl: '/hosting/ghpages/vhost',
-          buyDomainUrl: 'https://www.gandi.net',
-          skipVhostSelection: false,
-        }
-      ];
-      res.json({
-        skipProviderSelection: false,
-        providers: providers,
-      });
-    //.catch(err => {
-    //  res.status(400).send({
-    //    message: `Error while retrieving info about the github service with unifile`,
-    //    err: err,
-    //  });
-    //});
-  });
-  router.get('/hosting/ghpages/vhost', (req, res, next) => {
-      unifile.readdir(req.session.unifile, 'github', '/')
-      .then(result => {
-        res.json(result
-          .sort((a, b) => new Date(b.modified) - new Date(a.modified))
-          //.slice(0, 1) // max number of repos
-          .map(file => {
-            return {
-              name: file.name,
-              domainUrl: `/hosting/ghpages/vhost/${ file.name }`,
-              skipDomainSelection: false,
-              publicationPath: {
-                absPath: `/ce/github/get/${ file.name }/gh-pages`,
-                isDir: true,
-                mime: 'application/octet-stream',
-                name: 'gh-pages',
-                folder: file.name,
-                path: `${ file.name }/gh-pages`,
-                service: 'github',
-                url: `http://localhost:6805/ce/github/get/${ file.name }/gh-pages`,
-              }
-            };
-          })
-        );
-      })
-      .catch(err => {
-        res.status(400).send({
-          message: `Error from hosting provider "${ req.params.id }": ${ err.message }`,
-          err: err,
-        });
-      });
-  });
-  router.get('/hosting/ghpages/vhost/:name', (req, res, next) => {
-      unifile.readFile(req.session.unifile, 'github', `/${ req.params.name }/gh-pages/CNAME`)
-      .then(result => {
-        res.json({
-          domain: result.toString().replace(/\n/g, ''),
-          https: true,
-        });
-      })
-      .catch(err => {
-        res.json({
-          domain: `${ req.session.unifile.github.account.login }.github.io/${ req.params.name }`,
-          https: true,
-          msg: err,
-        });
-      });
-  });
-  router.post('/hosting/ghpages/vhost/:name', (req, res, next) => {
-    unifile.writeFile(req.session.unifile, 'github', `/${ req.params.name }/gh-pages/CNAME`, req.body.domain)
-      .then(result => {
-        res.json({
-          domain: req.body.domain,
-          https: true,
-        });
-      })
-      .catch(err => {
-        res.status(400).send({
-          message: `Error when trying to attach a domain to "${ req.params.id }". Error details: ${ err.message }`,
-          err: err,
-        });
-      });
-  });
-  router.delete('/hosting/ghpages/vhost/:name', (req, res, next) => {
-    function genericDomain() {
-      res.json({
-        domain: `${ req.session.unifile.github.account.login }.github.io/${ req.params.name }`,
-        https: true,
-      });
-    }
-    unifile.unlink(req.session.unifile, 'github', `/${ req.params.name }/gh-pages/CNAME`, req.body.domain)
-      .then(result => genericDomain())
-      .catch(err => {
-        if(err.code === 'ENOENT') {
-          // there was no CNAME file
-          genericDomain();
-        }
-        else {
-          res.status(400).send({
-            message: `Error when trying to remove domain from "${ req.params.name }". Error details: ${ err.message }`,
-            err: err,
-          });
-        }
-      });
+    res.json({
+      providers: res.locals.providers,
+      skipProviderSelection: skipProviderSelection,
+    });
   });
   return router;
 };
