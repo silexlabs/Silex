1bbb5e2ca0967abda9e74778554fb92ce07f4026
Alex Hoyau <a.hoyau@silexlabs.org>
2019-03-15 20:54:47 -0600

    fixed UI little bugs, added detail about upgrade of website

--- packages/silex-lib/dist/server/BackwardCompat.js
+++ packages/silex-lib/dist/server/BackwardCompat.js
@@ -11,6 +11,8 @@
 
 const Path = require('path');
 const constants = require('./Constants.json');
+// FIXME: path in constants
+const components = require('../../dist/client/libs/prodotype/components/components.json')
 
 /**
  * the version of the website is stored in the generator tag as "Silex v-X-Y-Z"
@@ -65,15 +67,29 @@ module.exports = class BackwardCompat {
       });
     }
     else if (hasToUpdate) {
+      const allActions = {};
       // convert to the latest version
-      return this.to2_2_8(version, doc).then(() => {
+      return this.to2_2_8(version, doc).then((actions) => {
+        if(actions.length) allActions['2.2.8'] = actions;
+      return this.to2_2_9(version, doc).then((actions) => {
+        if(actions.length) allActions['2.2.9'] = actions;
         // update the static scripts to match the current server and latest version
         this.updateStatic(doc);
         // store the latest version
         metaNode.setAttribute('content', 'Silex v' + LATEST_VERSION.join('.'));
-        // continue
+        // build the report for the user
+        const report = Object.keys(allActions).map(version => {
+          return `<p>Update to version ${ version }:
+              <ul>${ allActions[version].map(action => `<li class="no-list">${ action }</li>`).join('') }</ul>
+          </p>`
+        }).join('');
         // needs to reload if silex scripts and stylesheets have been updated
-        return 'This website has been updated with the latest version of Silex.<br><br>Before you save it, please check that everything is fine. Saving it with another name could be a good idea too (menu file > save as).';
+        return `<h3>This website has been updated with the latest version of Silex.</h3>
+        <p>Before you save it, please check that everything is fine. Saving it with another name could be a good idea too (menu file > save as).</p>
+        <p>Bellow you will find details of what I did.</p><hr/>
+          ${ report }
+        `;
+      })
       })
     }
     else {
@@ -151,35 +167,63 @@ module.exports = class BackwardCompat {
     return initialVersion[0] < targetVersion[0] ||
       initialVersion[1] < targetVersion[1] ||
       initialVersion[2] < targetVersion[2];
-  }
+    }
 
 
-  /**
-   * @param {Array.<number>} version
-   * @param {Document} doc
-   * @return {Promise} a Promise
-   */
-  to2_2_8(version, doc) {
-    // TODO when there will be BC again
-    return new Promise((resolve, reject) => {
-      if (this.hasToUpdate(version, [2, 2, 8])) {
-        console.log('updating', version, [2, 2, 8]);
-        // cleanup the hamburger menu icon
-        const menuButton = doc.querySelector('.menu-button')
-        if(menuButton) {
-          menuButton.classList.remove('paged-element', 'paged-element-hidden', 'page-page-1', 'prevent-resizable');
-          menuButton.classList.add('hide-on-desktop');
+    /**
+     * @param {Array.<number>} version
+     * @param {Document} doc
+     * @return {Promise} a Promise
+     */
+    to2_2_8(version, doc) {
+      return new Promise((resolve, reject) => {
+        const actions = [];
+        if (this.hasToUpdate(version, [2, 2, 8])) {
+          console.log('updating', version, [2, 2, 8]);
+          // cleanup the hamburger menu icon
+          const menuButton = doc.querySelector('.menu-button')
+          if(menuButton) {
+            menuButton.classList.remove('paged-element', 'paged-element-hidden', 'page-page-1', 'prevent-resizable');
+            menuButton.classList.add('hide-on-desktop');
+          }
+          // give the hamburger menu a size (TODO: add to the json model too)
+          doc.querySelector('.silex-inline-styles').innerHTML += '.silex-id-hamburger-menu {width: 50px;min-height: 40px;}'
+          // pages need to have href set
+          Array.from(doc.querySelectorAll('.page-element'))
+          .forEach(el => {
+            el.setAttribute('href', '#!' + el.getAttribute('id'));
+            console.log('set href of hamburger menu page', el.href);
+          });
+          actions.push(`
+            <p>I fixed the mobile menu so that it is compatible with the new publication (now multiple pages are generated instead of 1 single page for the whole website).</p>
+          `);
         }
-        // give the hamburger menu a size (TODO: add to the json model too)
-        doc.querySelector('.silex-inline-styles').innerHTML += '.silex-id-hamburger-menu {width: 50px;min-height: 40px;}'
-        // pages need to have href set
-        Array.from(doc.querySelectorAll('.page-element'))
-        .forEach(el => {
-          el.setAttribute('href', '#!' + el.getAttribute('id'));
-          console.log('set href of hamburger menu page', el.href);
-        });
-      }
-      resolve();
-    });
-  }
+        resolve(actions);
+      });
+    }
+
+
+    /**
+     * @param {Array.<number>} version
+     * @param {Document} doc
+     * @return {Promise} a Promise
+     */
+    to2_2_9(version, doc) {
+      return new Promise((resolve, reject) => {
+        let actions = [];
+        if (this.hasToUpdate(version, [2, 2, 9])) {
+          console.log('updating', version, [2, 2, 9]);
+          // remove the hamburger menu icon
+          const menuButton = doc.querySelector('.menu-button')
+          if(menuButton) {
+            menuButton.parentNode.removeChild(menuButton);
+            actions.push(`
+              <p>I removed the mobile menu as there is now a component for that.</p>
+              <p><a target="_blank" href="https://github.com/silexlabs/Silex/wiki/Hamburger-menu">Read more about the Hamburger Menu component here</a>.</p>
+            `);
+          }
+        }
+        resolve(actions);
+      });
+    }
 }
