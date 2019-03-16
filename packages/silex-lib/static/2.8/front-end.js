765e2e30fd1915e5376dd7909ae2776bd43da82d
Alex Hoyau <a.hoyau@silexlabs.org>
2019-03-15 21:03:02 -0600

    static scripts: created 2.9 and reverted 2.8

--- packages/silex-lib/static/2.8/front-end.js
+++ packages/silex-lib/static/2.8/front-end.js
@@ -9,216 +9,161 @@
 // http://www.silexlabs.org/silex/silex-licensing/
 //////////////////////////////////////////////////
 $(function() {
+  var $win = $(window);
+
+  // expose data to components
+  window.silex = window.silex || {};
+
   // allow HTML5 tags used by Silex to be styled with CSS (polyfill)
   document.createElement('HEADER');
   document.createElement('VIDEO');
 
-  // store jquery references
-  var $win = $(window);
-  var $doc = $(document);
+  // store the body selector
+  // be careful since it will change after undo/redo or open file in Silex editor
   var $body = $('body');
-  var siteWidth = parseInt($('meta[name=website-width]').attr('content')) || null;
 
-  // expose data to components
-  window.silex = window.silex || {};
-  window.silex.siteWidth = siteWidth;
-  window.silex.scale = 1;
-  window.silex.scroll = {top: 0, left: 0};
+  /**
+   * window resize event
+   */
+  var siteWidth = parseInt($('meta[name=website-width]').attr('content') || '480');
+  var resizeBody = function (event){
+    var $html = $('html');
+    window.silex.resizeRatio = 1;
 
-  // FIXME: why is it needed?
-  window.silex.resizeBody = function() {};
-
-  // this script is only for outside the editor
-  if($body.hasClass('silex-runtime')) {
-    if(!$body.hasClass('silex-published')) {
-      initPages();
-    }
-    onScroll();
-    onResize();
-    $win.resize(onResize);
-    $win.scroll(onScroll);
-    // if(!$body.hasClass('silex-published')) {
-    //   $body.on('pageChanged', function() {
-    //     initFixedPositions();
-    //     onScroll();
-    //     onResize();
-    //   });
-    // }
-    // cross browser get scroll (even IE)
-
-    function getScroll() {
-      var container = $(document.scrollingElement || 'html');
-      return {
-        left: container.scrollLeft(),
-        top: container.scrollTop()
-      }
-    }
-
-    function onResize() {
+    // behavior which is not the same in Silex editor and outside the editor
+    if($body.hasClass('silex-runtime')) {
       // if the site has a defined width and the window is smaller than this width, then
       // scale the website to fit the window
-      // This happens also on mobile
 
-      // scale on mobile or on desktop only when needed
-      var ratio = getScaleRatio();
-      var scroll = getScroll();
+      var scrollRatio = $body.scrollTop() / $body.prop("scrollHeight");
 
-      // notify the components that the resize will occure
-      $doc.trigger('silex.preresize', {
-        scrollTop: scroll.top/ratio,
-        scrollLeft: scroll.left/ratio,
-        scale: ratio
-      });
-      window.data = {
-        scrollTop: scroll.top/ratio,
-        scrollLeft: scroll.left/ratio,
-        scale: ratio
-      };
+      // reset transform
+      // needed to measure window width
+      $html.css({
+        'transform': '',
+        'transform-origin': '',
+        'min-width': '',
+      })
+      var winWidth = $win.width();
 
-      // expose the ratio to components
-      window.silex.scale = ratio;
-      window.silex.scrollTop = scroll.top;
-      window.silex.scrollLeft = scroll.left;
-
-      if(ratio === 1) {
-        // reset scale
-        $body.css({
-          'transform': '',
-          'transform-origin': '',
-          'min-width': '',
-          'height': ''
-        });
-        // unscale some elements
-        $('.prevent-scale').css({
-          'transform': '',
-          'transform-origin': ''
-        })
-      }
-      else {
-        // scale the body
-        $body.css({
+      // handle resize when needed
+      if(winWidth < siteWidth) {
+        var height = $html.height();
+        // scale the site
+        var breakPoint = winWidth < 480 ? 480 : siteWidth;
+        var ratio = winWidth / breakPoint;
+        $html.css({
           'transform': 'scale(' + ratio + ')',
           'transform-origin': '0 0',
-          'min-width': getScaleBreakPoint() + 'px',
-          'height': $body.height() * ratio
-        });
-        // unscale some elements
-        $('.prevent-scale').css({
-          'transform': 'scale(' + (1/ratio) + ')',
-          'transform-origin': '0 0'
+          'min-width': breakPoint + 'px',
+          'height': height * ratio,
         })
-      }
-      $doc.trigger('silex.resize', {
-        scrollTop: scroll.top/ratio,
-        scrollLeft: scroll.left/ratio,
-        scale: ratio
-      });
-    }
+        // expose the ratio to components
+        window.silex.resizeRatio = ratio;
 
-    function onScroll() {
-      // simulate the fixed position
-      var ratio = getScaleRatio();
-      var scroll = getScroll();
-      $doc.trigger('silex.prescroll', {
-        scrollTop: scroll.top/ratio,
-        scrollLeft: scroll.left/ratio,
-        scale: ratio
-      });
-      window.data = {
-        scrollTop: scroll.top/ratio,
-        scrollLeft: scroll.left/ratio,
-        scale: ratio
-      };
-      var offsetTop = scroll.top / ratio;
-      var offsetLeft = scroll.left / ratio;
-      $('.fixed').css({
-        'position': '',
-        'transform': 'translate(' + offsetLeft + 'px, ' + offsetTop + 'px)',
-        'transform-origin': '0 0'
-      });
-      $('.fixed.prevent-scale').css({
-        'position': '',
-        'transform': 'translate(' + offsetLeft + 'px, ' + offsetTop + 'px) scale(' + (1/ratio) + ')',
-        'transform-origin': '0 0'
-      });
-      $doc.trigger('silex.scroll', {
-        scrollTop: scroll.top/ratio,
-        scrollLeft: scroll.left/ratio,
-        scale: ratio
-      });
-    }
-
-    // utility functions
-    function getScaleRatio() {
-      var winWidth = $win.width();
-      if((siteWidth && winWidth < siteWidth) || winWidth < 480) {
-        // scale the site
-        var breakPoint = getScaleBreakPoint();
-        return winWidth / breakPoint;
+        // keep the scroll position when resizing,
+        // fixes a bug on mobile when reaching the bottom of page and the broser UI comes back and changes the viewport size
+        var scrollTarget = scrollRatio * $body.prop("scrollHeight");
+        $body.scrollTop(scrollTarget);
       }
-      return 1;
     }
-    function getScaleBreakPoint() {
-      var winWidth = $win.width();
-      return winWidth < 480 ? 480 : siteWidth;
+    else {
+      // add space around the elements in the body
+      // I removed this because it bugs when there are elements with 100% width
+      //width += 50;
+      //height += 50;
     }
+    // dispatch an event so that components can update
+    $(document).trigger('silex:resize');
+  };
 
-    function initPages() {
-      /**
-       * list all pages from the head section
-       * and open the 1st one by default
-       */
-      var firstPageName = null;
-      var pages = $('a[data-silex-type="page"]');
-      if (pages && pages.length>0){
-        var firstPage = pages[0];
-        firstPageName = firstPage.getAttribute('id');
-      }
-      /**
-       * callback for change events
-       * called when a page is opened
-       */
-      $body.on('pageChanged', function (event, pageName) {
-        // mark links to the current page as active
-        $('[data-silex-href*="#!'+pageName+'"]').addClass('page-link-active');
-        $('[id*="'+pageName+'"]').addClass('page-link-active');
-        // prevent iframe content from staying in the dom
-        // this prevent a youtube video to continue playing while on another page
-        // this is useful in chrome and not firefox since display:none does not reset iframe dom in chrome
-        $('[data-silex-iframe-src]').each(function() {
-          this.setAttribute('src', this.getAttribute('data-silex-iframe-src'));
-        });
-        $('.paged-element-hidden iframe').each(function() {
-          var src = this.getAttribute('src');
-          if(src) {
-            this.setAttribute('data-silex-iframe-src', src);
-            this.setAttribute('src', '');
-          }
-        });
-      });
-      /**
-       * init page system
-       * Use deep links (hash) only when `body.silex-runtime` is defined, i.e. not while editing
-       */
-      $body.pageable({
-        currentPage: firstPageName,
-        useDeeplink: $body.hasClass('silex-runtime'),
-        pageClass: 'paged-element'
-      });
-      /**
-       * Silex links
-       * Only when `body.silex-runtime` is defined, i.e. not while editing
-       * Links are not clickable while editing
-       */
-      $('.silex-runtime [data-silex-href]').click(function () {
-        var href = this.getAttribute('data-silex-href');
-        if (href.indexOf('#') === 0){
-          window.location.href = href;
-        }
-        else {
-          window.open(href, '_blank');
-        }
-      });
+  /* this doesn't work? at least not in google bot mobile
+  // only outside silex editor when the window is small enough
+  // change viewport to enable mobile view scale mode
+  // for "pixel perfect" mobile version
+  // bellow 960, the window width will be seen as 480
+  if($body.hasClass('silex-runtime')) {
+    var winWidth = win.width();
+    if(winWidth < 960) {
+      $('meta[data-silex-viewport]').attr('content', 'width=479, user-scalable=no, maximum-scale=1');
     }
   }
+  */
+  if(!$body.hasClass('silex-published')) {
+    /**
+     * list all pages from the head section
+     * and open the 1st one by default
+     */
+    var firstPageName = null;
+    var pages = $('a[data-silex-type="page"]');
+    if (pages && pages.length>0){
+      var firstPage = pages[0];
+      firstPageName = firstPage.getAttribute('id');
+    }
+    /**
+     * callback for change events
+     * called when a page is opened
+     */
+    $body.on('pageChanged', function (event, pageName) {
+      // mark links to the current page as active
+      $('[data-silex-href*="#!'+pageName+'"]').addClass('page-link-active');
+      $('[id*="'+pageName+'"]').addClass('page-link-active');
+      // prevent iframe content from staying in the dom
+      // this prevent a youtube video to continue playing while on another page
+      // this is useful in chrome and not firefox since display:none does not reset iframe dom in chrome
+      $('[data-silex-iframe-src]').each(function() {
+        this.setAttribute('src', this.getAttribute('data-silex-iframe-src'));
+      });
+      $('.paged-element-hidden iframe').each(function() {
+        var src = this.getAttribute('src');
+        if(src) {
+          this.setAttribute('data-silex-iframe-src', src);
+          this.setAttribute('src', '');
+        }
+      });
+      // resize on page change (size will vary)
+      resizeBody();
+    });
+    /**
+     * init page system
+     * Use deep links (hash) only when `body.silex-runtime` is defined, i.e. not while editing
+     */
+    $body.pageable({
+      currentPage: firstPageName,
+      useDeeplink: $body.hasClass('silex-runtime'),
+      pageClass: 'paged-element'
+    });
+    /**
+     * Silex links
+     * Only when `body.silex-runtime` is defined, i.e. not while editing
+     * Links are not clickable while editing
+     */
+    $('.silex-runtime [data-silex-href]').click(function () {
+      var href = this.getAttribute('data-silex-href');
+      if (href.indexOf('#') === 0){
+        window.location.href = href;
+      }
+      else {
+        window.open(href, '_blank');
+      }
+    });
+  }
+  /**
+   * mobile menu
+   */
+  $('.silex-runtime.enable-mobile .silex-pages .menu-button').click(function (e) {
+    e.stopPropagation();
+    $(document.body).toggleClass('show-mobile-menu');
+  });
+  $('.silex-runtime.enable-mobile').click(function (e) {
+    $(document.body).removeClass('show-mobile-menu');
+  });
+  // expose for use by the widgets and Silex editor
+  window.silex.resizeBody = resizeBody;
+
+  // resize body at start
+  resizeBody();
+
+  // resize body on window resize
+  $win.resize(resizeBody);
 });
