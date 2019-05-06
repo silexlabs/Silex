89bae78f4126e329f5711732e0d259672b00e2b4
Alex Hoyau <a.hoyau@silexlabs.org>
2019-05-06 12:42:01 -0500

    fixed default exports for silex as an npm dependency

---
+++ src/server/index.ts
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////
+// Silex, live web creation
+// http://projects.silexlabs.org/?/silex/
+//
+// Copyright (c) 2012 Silex Labs
+// http://www.silexlabs.org/
+//
+// Silex is available under the GPL license
+// http://www.silexlabs.org/silex/silex-licensing/
+//////////////////////////////////////////////////
+
+'use strict';
+
+import SilexServer from './SilexServer';
+import { Config } from './ServerConfig';
+
+export {
+  Config,
+  SilexServer,
+}
