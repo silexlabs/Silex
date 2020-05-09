d3efca25cfd44ebcbf1ec15d267b647d77cab2c5
Alex Hoyau <a.hoyau@silexlabs.org>
2020-05-09 19:32:50 +0200

    lint

--- src/ts/client/expose.ts
+++ src/ts/client/expose.ts
@@ -2,6 +2,7 @@
 // Expose the API to JS
 // TODO: expose file, copy, element, undo, page, view,
 export function exposeModule(win: Window, obj: any) {
+  // tslint:disable
   win['silex'] = {
     ...win['silex'],
     ...obj,
