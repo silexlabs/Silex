3cc11f134f9430774751696c38e8c35b7fefc334
Alex Hoyau <alex@lexoyo.me>
2022-03-12 20:14:36 +0100

    revert WIP: trying to figure out bug #1169

--- src/ts/client/components/SiteFrame.ts
+++ src/ts/client/components/SiteFrame.ts
@@ -102,6 +102,17 @@ class Site {
     const contentDocument = getSiteDocument()
     const contentWindow = getSiteWindow()
 
+    // wait for the webste to be loaded
+    // can not rely on the load event of the iframe because there may be missing
+    // assets
+    // tslint:disable:no-string-literal
+    if (contentDocument.body === null || contentWindow === null || contentWindow['jQuery'] === null) {
+      setTimeout(() => {
+        this.contentChanged(opt_cbk)
+      })
+      return
+    }
+
     // notify the caller
     if (opt_cbk) {
       opt_cbk()
