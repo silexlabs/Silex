285849d3161dd768be965dce8445957d67291e6b
Alex Hoyau <alex@lexoyo.me>
2023-01-11 18:37:39 +0100

    wip: symbols plugin

---
+++ packages/silex-lib/src/ts/client/grapesjs/symbolDialogs.ts
@@ -0,0 +1,21 @@
+import * as grapesjs from 'grapesjs/dist/grapes.min.js'
+import { cmdAddSymbol } from '@silexlabs/grapesjs-symbols'
+
+export const cmdPromptAddSymbol = 'symbol-prompt-add'
+
+export default grapesjs.plugins.add(name, (editor, opts) => {
+  function getNext(prefix) {
+    let idx = 1
+    while(editor.Symbols.find(s => s.get('label') === prefix + idx)) {
+      idx++
+    }
+    return prefix + idx
+  }
+  editor.Commands.add(cmdPromptAddSymbol, {
+    run: (_, sender) => {
+      const label = prompt('Label', getNext('Symbol '))
+      const icon = prompt('Icon', 'fa-list')
+      editor.runCommand(cmdAddSymbol, { label, icon })
+    }
+  })
+})
