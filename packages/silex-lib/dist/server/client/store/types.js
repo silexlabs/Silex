161295e6bbc33f6a2e8c635aceef0b366bc33d08
Alex Hoyau <alex@lexoyo.me>
2023-02-11 15:03:26 +0100

    revert: include built files in git

--- packages/silex-lib/dist/server/client/store/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * @fileoverview Type definitions. Cross platform, it needs to run client and server side
- *
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidHlwZXMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi9zcmMvdHMvY2xpZW50L3N0b3JlL3R5cGVzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQTs7O0dBR0cifQ==
