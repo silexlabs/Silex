963e30061b5a10acba05950671fa6240dcc20b60
Alex Hoyau <alex@lexoyo.me>
2023-02-11 15:03:26 +0100

    revert: include built files in git

--- dist/server/client/site-store/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * @fileoverview Type definitions. Cross platform, it needs to run client and server side
- *
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidHlwZXMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi9zcmMvdHMvY2xpZW50L3NpdGUtc3RvcmUvdHlwZXMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBOzs7R0FHRyJ9
