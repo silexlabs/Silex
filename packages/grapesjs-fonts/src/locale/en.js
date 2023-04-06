c586b86c28d31ba0138db81dfeddbbad783e6283
Alex Hoyau <alex@lexoyo.me>
2023-04-06 11:12:03 +0000

    lint

--- packages/grapesjs-fonts/src/locale/en.js
+++ packages/grapesjs-fonts/src/locale/en.js
@@ -1,13 +1,13 @@
 export default {
     'grapesjs-fonts': {
-      'Fonts': 'Fonts',
-      'You must provide Google font api key': 'You must provide Google font api key, see https://developers.google.com/fonts/docs/developer_api#APIKey',
-      'Search': 'Search fonts...',
-      'Add font': 'Install font',
-      'Remove': 'Remove',
-      'Save': 'Save',
-      'Cancel': 'Cancel',
-      'Installed fonts': 'Installed fonts',
+        'Fonts': 'Fonts',
+        'You must provide Google font api key': 'You must provide Google font api key, see https://developers.google.com/fonts/docs/developer_api#APIKey',
+        'Search': 'Search fonts...',
+        'Add font': 'Install font',
+        'Remove': 'Remove',
+        'Save': 'Save',
+        'Cancel': 'Cancel',
+        'Installed fonts': 'Installed fonts',
     },
 }
 
--- packages/grapesjs-fonts/src/locale/fr.js
+++ packages/grapesjs-fonts/src/locale/fr.js
@@ -1,14 +1,14 @@
 export default {
     'grapesjs-fonts': {
-      'Fonts': 'Polices',
-      'You must provide Google font api key': 'Vous devez fournir une clé API Google font, voir https://developers.google.com/fonts/docs/developer_api#APIKey',
-      'Family': 'Famille',
-      'Search': 'Rechercher une police',
-      'Add font': 'Installer la police',
-      'Remove': 'Supprimer',
-      'Save': 'Enregistrer',
-      'Cancel': 'Annuler',
-      'Installed fonts': 'Polices installées',
+        'Fonts': 'Polices',
+        'You must provide Google font api key': 'Vous devez fournir une clé API Google font, voir https://developers.google.com/fonts/docs/developer_api#APIKey',
+        'Family': 'Famille',
+        'Search': 'Rechercher une police',
+        'Add font': 'Installer la police',
+        'Remove': 'Supprimer',
+        'Save': 'Enregistrer',
+        'Cancel': 'Annuler',
+        'Installed fonts': 'Polices installées',
     },
 }
 
