324ebe46fb951b3bb273a915ebc6514d3df09117
Alex Hoyau <alex.hoyau@internet2000.net>
2023-06-15 08:05:02 +0200

    add missing cli params, cleanup blocks

--- src/ts/client/grapesjs/blocks.ts
+++ src/ts/client/grapesjs/blocks.ts
@@ -1,88 +1,20 @@
-import * as grapesjs from 'grapesjs/dist/grapes.min.js'
-
-const pluginName = 'blocks'
-const clsContainer = 'silex-container'
-const clsSection = 'silex-section'
-const clsSectionBackground = 'silex-section-background'
-
-export const blocksPlugin = grapesjs.plugins.add(pluginName, (editor, opts) => {
-  // Make row and column classes private
-  const privateCls = [clsSection, clsSectionBackground, clsContainer]
-    .map(cls => '.' + cls)
-  editor.on(
-    'selector:add',
-    selector => privateCls.indexOf(selector.getFullName()) >= 0 && selector.set('private', 1)
-  )
-  ;[
-    // {
-    //   id: 'section',
-    //   def: {
-    //     label: 'Section',
-    //     category: 'Containers',
-    //     attributes: { class: 'fa fa-list-alt' },
-    //     draggable: 'body',
-    //     content: {
-    //       tagName: 'section',
-    //       styles: `
-    //       .${clsSectionBackground} {
-    //         display: flex;
-    //         justify-content: center;
-    //       }
-    //       `,
-    //       components: [
-    //         {
-    //           tagName: 'div',
-    //           name: 'Section container',
-    //           attributes: { class: ['website-width'] },
-    //           draggable: false,
-    //           removable: false,
-    //           resizable: true,
-    //           styles: `
-    //           .${clsSection} {
-    //             min-height: 100px;
-    //             width: 1200px;
-    //           }
-    //           `,
-    //         },
-    //       ],
-    //     },
-    //   },
-    // },
-    {
-      id: 'container',
-      def: {
-        label: 'Container',
-        category: 'Basics',
-        attributes: { class: 'container-png' },
-        content: {
-          type: 'container',
-          style: {
-            'min-height': '100px',
-          },
-        },
+export const blocksPlugin = (editor, opts) => {
+  // Defaults
+  const options = {
+    ...opts,
+  }
+  // Container block
+  const containerId = 'container'
+  editor.BlockManager.add(containerId, {
+    label: 'Container',
+    category: 'Basics',
+    attributes: { class: 'container-png' },
+    content: {
+      type: containerId, // display as "Container"
+      style: {
+        'min-height': '100px',
       },
     },
-    // {
-    //   id: 'link-block',
-    //   def: {
-    //     category: 'Containers',
-    //     label: 'Link Block',
-    //     attributes: { class: 'fa fa-link' },
-    //     content: {
-    //       type: 'link',
-    //       editable: false,
-    //       droppable: true,
-    //       resizable: true,
-    //       style:{
-    //         display: 'inline-block',
-    //         padding: '5px',
-    //         'min-height': '100px',
-    //         'width': '100px'
-    //       }
-    //     },
-    //   },
-    // },
-  ]
-    .forEach(block => editor.BlockManager.add(block.id, block.def))
-})
-
+  })
+  editor.DomComponents.addType(containerId, {})
+}
