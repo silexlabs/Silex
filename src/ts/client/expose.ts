4a9968a6f6c577dbd5248a4065f24e5088a6ead1
Alex Hoyau <a.hoyau@silexlabs.org>
2023-07-13 11:20:10 +0200

    cleanup, refactoring events

--- src/ts/client/expose.ts
+++ src/ts/client/expose.ts
@@ -22,7 +22,7 @@ export * from './config'
 export * as constants from '../constants'
 export * as utils from './utils'
 export * as api from './api'
-export * as events from '../events'
+export * as events from './events'
 export * as page from '../page'
 export * as types from '../types'
 
