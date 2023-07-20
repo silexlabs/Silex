e0ad428e1a8beef8837f107e0c8942825dd0d4a2
Alex Hoyau <a.hoyau@silexlabs.org>
2023-07-20 17:22:19 +0200

    versions, add plugin website info

--- packages/silex-platform/client-config.js
+++ packages/silex-platform/client-config.js
@@ -1,5 +1,8 @@
 // This file is loaded by Silex when the user opens the editor
 // Its path is set in the environment variable SILEX_CLIENT_CONFIG in index.js
+import websiteInfoPlugin from './plugins/client/website-info.js'
+
 export default async function (config) {
+    config.addPlugin(websiteInfoPlugin, {})
     return {}
 }
