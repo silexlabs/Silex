34f59e5047c65416224ea607c19be55aac6a1360
Alex Hoyau <a.hoyau@silexlabs.org>
2023-07-26 15:26:00 +0200

    missing file

---
+++ src/ts/client/assetUrl.ts
@@ -0,0 +1,182 @@
+import { API_PATH, API_WEBSITE_ASSET_READ, API_WEBSITE_PATH } from '../constants'
+import { Asset, Component, ConnectorId, Page, Style, WebsiteId } from '../types'
+
+// Orging and path
+const SERVER_URL = `${window.location.origin}${window.location.pathname}`
+
+/**
+ * Function to convert a path from it stored version to the one we give to grapesjs
+ * Stored version is like `/assets/image.webp`
+ * Grapesjs version is like `/api/website/assets/image.webp?websiteId=47868975&connectorId=gitlab`
+ * @param path the path to convert
+ * @param websiteId the website ID
+ * @param storageId the storage connector ID
+ * @returns the converted path
+ */
+function storedToDisplayed(path: string, websiteId: WebsiteId, storageId: ConnectorId): string {
+  const url = new URL(path, SERVER_URL)
+  url.pathname = url.pathname.replace(/^\/assets/, '')
+  url.pathname = `${API_PATH}${API_WEBSITE_PATH}${API_WEBSITE_ASSET_READ}${url.pathname}`
+  url.searchParams.set('websiteId', websiteId)
+  url.searchParams.set('connectorId', storageId)
+  const result = '/' + url.toString().replace(new RegExp(`^${SERVER_URL}`), '')
+  return result
+}
+
+/**
+ * Function to convert a path from the one we give to grapesjs to the stored version
+ * Grapesjs version is like `/api/website/assets/image.webp?websiteId=47868975&connectorId=gitlab`
+ * Stored version is like `/assets/image.webp`
+ * @param path the path to convert
+ * @returns the converted path
+ */
+function displayedToStored(path: string): string {
+  const url = new URL(path, SERVER_URL)
+  url.pathname = url.pathname.replace(new RegExp(`^${API_PATH}${API_WEBSITE_PATH}${API_WEBSITE_ASSET_READ}`), '')
+  url.pathname = `/assets${url.pathname}`
+  url.searchParams.delete('websiteId')
+  url.searchParams.delete('connectorId')
+  const result = '/' + url.toString().replace(new RegExp(`^${SERVER_URL}`), '')
+  return result
+}
+
+
+/**
+ * Update asset URL to use the current storage connector and website ID
+ * Assets URLs are stored in the project data in the form `/assets/image.webp`
+ * This function adds the API path and the connectorId and websiteId like so: `/api/website/assets/image.webp?websiteId=47868975&connectorId=gitlab`
+ */
+export function addTempDataToAssetUrl(assets: Asset[], websiteId: WebsiteId, storageId: ConnectorId): Asset[] {
+  return assets.map((asset: Asset) => {
+    return {
+      ...asset,
+      src: storedToDisplayed(asset.src, websiteId, storageId),
+    }
+  })
+}
+
+/**
+ * Remove the temporary data from the asset URL
+ * Remove the API path and the connectorId and websiteId like so: `/assets/image.webp`
+ */
+export function removeTempDataFromAssetUrl(assets: Asset[]): Asset[] {
+  return assets.map((asset: Asset) => {
+    // Return the asset with the new URL
+    return {
+      ...asset,
+      src: displayedToStored(asset.src),
+    }
+  })
+}
+
+/**
+ * Reucrsive function to update the asset URL in the components
+ */
+function addTempDataToComponents(component: Component, websiteId: WebsiteId, storageId: ConnectorId): Component {
+  // Update the asset URL in the component
+  const newComponent = {
+    ...component,
+    attributes: {
+      ...component.attributes,
+      src: component.attributes?.src ? storedToDisplayed(component.attributes.src, websiteId, storageId) : undefined,
+    },
+  }
+  // Update the asset URL in the children
+  if (newComponent.components) {
+    newComponent.components = newComponent.components.map((childComponent: Component) => {
+      return addTempDataToComponents(childComponent, websiteId, storageId)
+    })
+  }
+  return newComponent
+}
+
+/**
+ * Reucrsive function to update the asset URL in the components
+ */
+function removeTempDataFromComponents(component: Component): Component {
+  // Update the asset URL in the component
+  const newComponent = {
+    ...component,
+    attributes: {
+      ...component.attributes,
+      src: component.attributes?.src ? displayedToStored(component.attributes.src) : undefined,
+    },
+  }
+  // Update the asset URL in the children
+  if (newComponent.components) {
+    newComponent.components = newComponent.components.map((childComponent: Component) => {
+      return removeTempDataFromComponents(childComponent)
+    })
+  }
+  return newComponent
+}
+
+/**
+ * Add temp data to pages
+ */
+export function addTemDataToPages(pages: Page[], websiteId: WebsiteId, storageId: ConnectorId): Page[] {
+  return pages.map((page: Page) => {
+    return {
+      ...page,
+      frames: page.frames.map(frame => ({
+        ...frame,
+        component: addTempDataToComponents(frame.component, websiteId, storageId),
+      }))
+    }
+  })
+}
+
+/**
+ * Remove temp data from asset URL in the components
+ */
+export function removeTempDataFromPages(pages: Page[]): Page[] {
+  return pages.map((page: Page) => {
+    return {
+      ...page,
+      frames: page.frames.map(frame => ({
+        ...frame,
+        component: removeTempDataFromComponents(frame.component),
+      }))
+    }
+  })
+}
+
+/**
+ * Add data to stylesheets
+ */
+export function addTempDataToStyles(styles: Style[], websiteId: WebsiteId, storageId: ConnectorId): Style[] {
+  const pattern = /url\((["']?)([^"']+?)\1\)/g
+  return styles.map((style: Style) => {
+    if (style.style && style.style['background-image']) {
+      const urls = [...style.style['background-image'].matchAll(pattern)].map(match => storedToDisplayed(match[2], websiteId, storageId))
+      style = {
+        ...style,
+        style: {
+          ...style.style,
+          'background-image': urls.map(url => `url('${url}')`).join(', '),
+        },
+      }
+    }
+    return style
+  })
+}
+
+/**
+ * Remove temp data from stylesheets
+ */
+export function removeTempDataFromStyles(styles: Style[]): Style[] {
+  const pattern = /url\((["']?)([^"']+?)\1\)/g
+  return styles.map((style: Style) => {
+    if (style.style && style.style['background-image']) {
+      const urls = [...style.style['background-image'].matchAll(pattern)].map(match => displayedToStored(match[2]))
+      style = {
+        ...style,
+        style: {
+          ...style.style,
+          'background-image': urls.map(url => `url('${url}')`).join(', '),
+        },
+      }
+    }
+    return style
+  })
+}
