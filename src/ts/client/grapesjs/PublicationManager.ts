57317966155229bfb57ee4526d1de28f5216b66f
Alex Hoyau <a.hoyau@silexlabs.org>
2023-08-09 18:49:19 +0200

    fix error in gitlab api

--- src/ts/client/grapesjs/PublicationManager.ts
+++ src/ts/client/grapesjs/PublicationManager.ts
@@ -326,8 +326,8 @@ ${pageSettings?.head || ''}
 <title>${getSetting('title')}</title>
 <link rel="icon" href="${getSetting('favicon')}" />
 ${['description', 'og:title', 'og:description', 'og:image']
-.map(prop => `<meta property="${prop}" content="${getSetting(prop)}"/>`)
-.join('\n')
+    .map(prop => `<meta property="${prop}" content="${getSetting(prop)}"/>`)
+    .join('\n')
 }
 </head>
 ${body}
