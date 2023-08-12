387313bbcf212026e27908a4e85682e45ea839ae
Eskeminha <91256149+Eskeminha@users.noreply.github.com>
2023-08-12 14:02:41 -0300

    Create ptbr.js
    pt-BR Locale

---
+++ packages/grapesjs-fonts/src/locale/ptbr.js
@@ -0,0 +1,12 @@
+export default {
+    'grapesjs-fonts': {
+        'Fonts': 'Fontes',
+        'You must provide Google font api key': 'Você deve fornecer a chave API da fonte do Google, acesse https://developers.google.com/fonts/docs/developer_api?hl=pt-br#APIKey',
+        'Search': 'Pesquisar fontes...',
+        'Add font': 'Instalar fonte',
+        'Remove': 'Remover',
+        'Save': 'Salvar',
+        'Cancel': 'Cancelar',
+        'Installed fonts': 'Fontes instaladas',
+    },
+}
