02798d71278a752513351ad24c32b508f0648c2f
Alex Hoyau <a.hoyau@silexlabs.org>
2023-11-02 12:50:12 +0300

    clieanup, doc

---
+++ scripts/generate-submodules-doc.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const fs = require('fs/promises')
+const ini = require('js-ini')
+
+async function main() {
+  try {
+    const data = await fs.readFile('.gitmodules', 'utf-8')
+    const parsedData = ini.parse(data)
+    const array = Object.keys(parsedData).map((key) => ({
+      // From submodule "submodules/..." to "..."
+      name: key.replace(/^submodule \"packages\//, '').replace(/\"$/, ''),
+      ...parsedData[key],
+    }))
+    let markdown = `
+# Silex packages
+
+| Name | Repo | Description |
+| ---- | ---- | ----------- |
+`
+    let readmeCount = 0
+    for(project of array) {
+      let title = project.name
+      let description = ''
+      try {
+        const readme = await fs.readFile(`${project.path}/README.md`, 'utf-8')
+        readmeCount++
+        const lines = readme.split('\n')
+        const titleIndex = lines.findIndex((line) => line.match(/^(#+)/))
+        title = titleIndex >= 0 ? lines[titleIndex].replace(/^(#+)/, '').trim() : project.name
+        description = titleIndex >= 0 ? lines.slice(titleIndex + 1).find((line) => line.length > 0) ?? '' : ''
+      } catch (err) {
+        if(err.code === 'ENOENT') continue
+        else throw err
+      }
+      markdown += `| ${title} | \`${project.url}\` | ${description} |\n`
+    }
+    console.log(markdown)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+main()
---
+++ scripts/insert-in-readme.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const fs = require('fs/promises')
+
+async function main(content, key) {
+  // Read README.md
+  const readme = await fs.readFile('README.md', 'utf-8')
+  const keyText = `> Auto generated ${key}`
+  // Replace content between keys
+  const lines = readme.split('\n')
+  const keyIndexStart = lines.findIndex((line) => line.trim() === keyText)
+  const keyIndexEnd = lines.findIndex((line, index) => index > keyIndexStart && line.trim() === keyText)
+  if(keyIndexStart < 0 || keyIndexEnd < 0) throw new Error(`Key not found in readme: ${key}`)
+  lines.splice(keyIndexStart + 1, keyIndexEnd - keyIndexStart - 1, content)
+  // Write README.md
+  await fs.writeFile('README.md', lines.join('\n'))
+}
+
+// Read piped content
+let content = ''
+process.stdin.on('data', (chunk) => {
+  content += chunk
+})
+process.stdin.on('end', () => {
+  main(content, process.argv[2])
+})
