fd6feff758c89838b9b64f76caecd8ae6d7e9965
Alex Hoyau <a.hoyau@silexlabs.org>
2023-11-19 14:25:07 +0300

    new design for settings dialog

--- packages/silex-lib/src/ts/client/grapesjs/settings.ts
+++ packages/silex-lib/src/ts/client/grapesjs/settings.ts
@@ -20,6 +20,21 @@ import {live} from 'lit-html/directives/live.js'
 
 import { WebsiteSettings } from '../../types'
 
+const menuItems = [{
+  id: 'general',
+  label: 'General',
+}, {
+  id: 'seo',
+  label: 'SEO',
+}, {
+  id: 'social',
+  label: 'Social',
+}, {
+  id: 'code',
+  label: 'Code',
+}]
+let currentMenuItem = menuItems[0]
+
 const idCodeWrapper = 'settings-head-wrapper'
 const el = document.createElement('div')
 let modal
@@ -106,75 +121,109 @@ function displaySettings(editor, config, model = editor.getModel()) {
           <p>These settings override the site settings, <a href="https://github.com/silexlabs/Silex/wiki/Settings" target="_blank">more info about settings here</a>.</p>
         `}
       </div>
-      <h2>General settings</h2>
-      <div class="silex-form__group col2">
-        <label class="silex-form__element">
-          <h3>${isSite(model) ? 'Site name' : 'Page name'}</h3>
-          <p>${isSite(model) ? 'The project name in the editor, for you and your team.' : 'Label of the page in the editor, and file name of the HTML page.'}</p>
-          <input type="text" name="name" .value=${live(model.get('name') || '')}/>
-        </label>
-        <label class="silex-form__element">
-          <h3>Website language</h3>
-          <p>This is the default language code for this website. Example: en, fr, es...</p>
-          <input type="text" name="lang" .value=${live(settings.lang || '')}/>
-        </label>
-        <label class="silex-form__element">
-          <h3>Title</h3>
-          <p>Title of the browser window, and title in the search engines results. It is used by search engines to find out what your site is talking about. The title should be a maximum of 70 characters long, including spaces.</p>
-          <input type="text" name="title" .value=${live(settings.title || '')}/>
-        </label>
-        <label class="silex-form__element">
-          <h3>Description</h3>
-          <p>Description displayed by the search engines in search results. It is used by search engines to find out what your site is talking about. It is best to keep meta descriptions between 150 and 160 characters.</p>
-          <input type="text" name="description" .value=${live(settings.description || '')}/>
-        </label>
-        <label class="silex-form__element">
-          <h3>Favicon</h3>
-          <p>Small image displayed in the browser's address bar and in tabs. The recommended size is 16×16 or 32×32 pixels. This can be a URL or a relative path.</p>
-          <input type="text" name="favicon" .value=${live(settings.favicon || '')}/>
-        </label>
-      </div>
-      <h2>Social networks sharing</h2>
-      <div>
-        <p>Once your website is live, you can use these tools to test sharing:&nbsp;<a target="_blank" href="https://developers.facebook.com/tools/debug/">Facebook</a>,
-        <a target="_blank" href="https://cards-dev.twitter.com/validator">Twitter</a>,
-        <a target="_blank" href="https://www.linkedin.com/post-inspector/inspect/">Linkedin</a></p>
-      </div>
-      <div class="silex-form__group col2">
-        <label class="silex-form__element">
-          <h3>Title</h3>
-          <p>The title of your website displayed when a user shares your website on a social network.
-        Do not include any branding in this title, just eye-catching phrase, e.g. "Learn everything about fishing".
-        Title should be between 60 and 90 characters long.</p>
-          <input type="text" name="og:title" .value=${live(settings['og:title'] || '')}/>
-        </label>
-        <label class="silex-form__element">
-          <h3>Description</h3>
-          <p>Description displayed when a user shares your website on a social network. Make it catchy, and invite readers to visit your website too, e.g. "Sam's website about fishing, check it out!" Title should be between 60 and 90 characters long.</p>
-          <input type="text" name="og:description" .value=${live(settings['og:description'] || '')}/>
-        </label>
-        <label class="silex-form__element">
-          <h3>Image</h3>
-          <p>Thumbnail image which is displayed when your website is shared on a social network. The optimal size is 1200×627 pixels. At this size, your thumbnail will be big and stand out from the crowd. But do not exceed the 5MB size limit. If you use an image that is smaller than 400 pixels x 209 pixels, it will render as a much smaller thumbnail.</p>
-          <p>Please enter the full URL here, e.g. "http://mysite.com/path/to/image.jpg"</p>
-          <input type="text" name="og:image" .value=${live(settings['og:image'] || '')}/>
-        </label>
-      </div>
-      <h2>Code</h2>
-      <div class="silex-form__group">
-        <label class="silex-form__element" id="${idCodeWrapper}">
-          <h3>HTML head</h3>
-          <p>HTML code which will be inserted in the HEAD tag.</p>
-        </label>
-      </div>
+      <section class="silex-sidebar-dialog">
+        <aside class="silex-bar">
+          <ul class="silex-list silex-list--menu">
+            ${menuItems.map(item => html`
+              <li
+                class=${item.id === currentMenuItem.id ? 'active' : ''}
+                @click=${e => {
+                  saveSettings(editor, model)
+                  currentMenuItem = item
+                  displaySettings(editor, model)
+                  e.preventDefault()
+                }}
+              >
+                ${item.label}
+              </li>
+            `)}
+          </ul>
+        </aside>
+        <main>
+          ${currentMenuItem.id === 'general' ? html`
+            <div class="gjs-sm-sector-title">General</div>
+            <div class="silex-form__group col2">
+              <label class="silex-form__element">
+                <h3>${isSite(model) ? 'Site name' : 'Page name'}</h3>
+                <p class="silex-help">${isSite(model) ? 'The project name in the editor, for you to remember what it is about.' : 'Label of the page in the editor, and file name of the published HTML page.'}</p>
+                <input type="text" name="name" .value=${live(model.get('name') || '')}/>
+              </label>
+              <label class="silex-form__element">
+                <h3>Website language</h3>
+                <p class="silex-help">This is the default language code for this website. Example: en, fr, es...</p>
+                <input type="text" name="lang" .value=${live(settings.lang || '')}/>
+              </label>
+            </div>
+          ` : ''}
+          ${currentMenuItem.id === 'seo' ? html`
+            <div class="gjs-sm-sector-title">SEO</div>
+            <div class="silex-form__group col2">
+              <label class="silex-form__element">
+                <h3>Title</h3>
+                <p class="silex-help">Title of the browser window, and title in the search engines results. It is used by search engines to find out what your site is talking about. The title should be a maximum of 70 characters long, including spaces.</p>
+                <input type="text" name="title" .value=${live(settings.title || '')}/>
+              </label>
+              <label class="silex-form__element">
+                <h3>Description</h3>
+                <p class="silex-help">Description displayed by the search engines in search results. It is used by search engines to find out what your site is talking about. It is best to keep meta descriptions between 150 and 160 characters.</p>
+                <input type="text" name="description" .value=${live(settings.description || '')}/>
+              </label>
+              <label class="silex-form__element">
+                <h3>Favicon</h3>
+                <p class="silex-help">Small image displayed in the browser's address bar and in tabs. The recommended size is 16×16 or 32×32 pixels. This can be a URL or a relative path.</p>
+                <input type="text" name="favicon" .value=${live(settings.favicon || '')}/>
+              </label>
+            </div>
+          ` : ''}
+          ${currentMenuItem.id === 'social' ? html`
+            <div class="gjs-sm-sector-title">Social</div>
+            <div class="silex-help">
+              <p>Once your website is live, you can use these tools to test sharing:&nbsp;<a target="_blank" href="https://developers.facebook.com/tools/debug/">Facebook</a>,
+              <a target="_blank" href="https://cards-dev.twitter.com/validator">Twitter</a>,
+              <a target="_blank" href="https://www.linkedin.com/post-inspector/inspect/">Linkedin</a></p>
+            </div>
+            <div class="silex-form__group col2">
+              <label class="silex-form__element">
+                <h3>Title</h3>
+                <p class="silex-help">The title of your website displayed when a user shares your website on a social network.
+              Do not include any branding in this title, just eye-catching phrase, e.g. "Learn everything about fishing".
+              Title should be between 60 and 90 characters long.</p>
+                <input type="text" name="og:title" .value=${live(settings['og:title'] || '')}/>
+              </label>
+              <label class="silex-form__element">
+                <h3>Description</h3>
+                <p class="silex-help">Description displayed when a user shares your website on a social network. Make it catchy, and invite readers to visit your website too, e.g. "Sam's website about fishing, check it out!" Title should be between 60 and 90 characters long.</p>
+                <input type="text" name="og:description" .value=${live(settings['og:description'] || '')}/>
+              </label>
+              <label class="silex-form__element">
+                <h3>Image</h3>
+                <div class="silex-help">
+                  <p>Thumbnail image which is displayed when your website is shared on a social network. The optimal size is 1200×627 pixels. At this size, your thumbnail will be big and stand out from the crowd. But do not exceed the 5MB size limit. If you use an image that is smaller than 400 pixels x 209 pixels, it will render as a much smaller thumbnail.</p>
+                  <p>Please enter the full URL here, e.g. "http://mysite.com/path/to/image.jpg"</p>
+                </div>
+                <input type="text" name="og:image" .value=${live(settings['og:image'] || '')}/>
+              </label>
+            </div>
+          ` : ''}
+          ${currentMenuItem.id === 'code' ? html`
+            <div class="gjs-sm-sector-title">Code</div>
+            <div class="silex-form__group">
+              <label class="silex-form__element" id="${idCodeWrapper}">
+                <h3>HTML head</h3>
+                <p class="silex-help">HTML code which will be inserted in the HEAD tag.</p>
+              </label>
+            </div>
+        ` : ''}
+        </main>
+      </section>
       <footer>
         <p class="silex-version">Silex ${version}</p>
-        <input class="silex-button" type="button" @click=${e => editor.stopCommand(cmdOpenSettings)} value="Cancel">
-        <input class="silex-button" type="submit" value="Ok">
+        <!-- <input class="silex-button" type="button" @click=${e => editor.stopCommand(cmdOpenSettings)} value="Cancel"> -->
+        <input class="silex-button" type="submit" value="Close">
       </footer>
     </form>
   `, el)
-  el.querySelector(`#${idCodeWrapper}`).appendChild(headEditor.getElement())
+  el.querySelector(`#${idCodeWrapper}`)?.appendChild(headEditor.getElement())
   headEditor.setContent(settings.head || '')
 }
 
