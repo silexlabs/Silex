163bdd98f19b01f1a0cc9b05ce3271500054cce5
Alex Hoyau <a.hoyau@silexlabs.org>
2023-12-19 14:50:02 +0300

    cleanup

--- packages/grapesjs-data-source/src/utils.ts
+++ packages/grapesjs-data-source/src/utils.ts
@@ -6,12 +6,18 @@ import { Component } from "grapesjs"
 
 /**
  * Get the display type of a field
+ * For the dropdown in expressions
+ * @example "String", "String [ ]", "String { }"
  */
 function getTypeDisplayName(typeIds: TypeId[], kind: FieldKind | null): string {
   const typeLabel = typeIds.join(', ')
   return kind === 'list' ? `${typeLabel} [ ]` : kind === 'object' ? `${typeLabel} { }` : typeLabel
 }
 
+/**
+ * Get the label for a token
+ * This is mostly about formatting a string for the dropdowns
+ */
 export function getTokenDisplayName(component: Component, token: Token, desiredNumChars = -1): string {
   switch (token.type) {
     case 'property': {
@@ -30,6 +36,10 @@ export function getTokenDisplayName(component: Component, token: Token, desiredN
   }
 }
 
+/**
+ * Group tokens by type
+ * This is used to create the groups in dropdowns
+ */
 export function groupByType(editor: DataSourceEditor, component: Component, completion: Token[], expression: Expression): Record<string, Token[]> {
   return completion
     .reduce((acc, token) => {
@@ -70,23 +80,6 @@ export function groupByType(editor: DataSourceEditor, component: Component, comp
       return acc
     }, {} as Record<string, Token[]>)
 }
-///**
-// * Add css styles to options forms
-// */
-//let _styles = ''
-//export function setOptionsFormStyles(styles: string) {
-//  _styles = styles
-//}
-//function addStyles(optionsForm: TemplateResult | null): TemplateResult {
-//  if(!optionsForm) return html``
-//  return html`
-//    <style>
-//      ${OPTIONS_STYLES}
-//      ${_styles}
-//    </style>
-//    ${optionsForm}
-//  `
-//}
 
 /**
  * Create a "fixed" token
@@ -112,249 +105,19 @@ export function getFixedToken(value: string): Token {
   }
 }
 
-///**
-// * Set the completion function of a steps selector
-// */
-//export function setCompletion(dataTree: DataTree, component: Component, stepsSelector: StepsSelector) {
-//  stepsSelector.completion = (steps: Step[]): Step[] => {
-//    // Current expression
-//    const expression = steps.map(step => step.meta.token)
-//    // Get the completion, which is a context
-//    // It is a list of tokens, each one can be added at the end of the expression
-//    const tokens = dataTree.getCompletion(component, expression)
-//    // Get the type for each token
-//    // The filters output types are resolved
-//    const fields = tokens
-//      .map(token => dataTree.getExpressionResultType(tokens.concat(token), component))
-//    // Convert the context to steps
-//    return fields
-//    .map((field, index) => {
-//      const prev = index > 0 ? fields[index - 1] : null
-//      return toStep(dataTree, field, prev, tokens[index], component)
-//    })
-//  }
-//}
-//
-///**
-// * Handle the change event of a steps selector
-// */
-//export function chagedStepsSelector(component: Component, name: string, label: string, stepsSelector: StepsSelector, exposed: boolean) {
-//  // Check input
-//  if(!component) throw new Error('Component is required')
-//  if(!name) throw new Error('Name is required')
-//  // Update the tokens with new options values
-//  const steps = enrichSteps(stepsSelector.steps)
-//  // Update the state
-//  setState(component, name, {
-//    label,
-//    expression: steps.map(step => step.meta.token),
-//  }, exposed)
-//  stepsSelector.steps = steps
-//}
-//
-///**
-// * Handle the "fixed" value case and the step meta
-// */
-//function enrichSteps(steps: Step[]): Step[] {
-//  return steps
-//    .map(step => {
-//      const token = step.type === 'fixed' ? getFixedToken(step.options?.value ?? '', 'String', step.options?.inputType)
-//        : step.meta?.token ?? getFixedToken(step.options?.value ?? '', 'String', step.options?.inputType) // Add a fixed string if the token is not found
-//      token.options = step.options
-//      return {
-//        ...step,
-//        meta: {
-//          ...step.meta,
-//          token,
-//        }
-//      }
-//    })
-//}
-//
-///**
-// * Convert an expression to steps
-// * This will resolve the types of filters
-// */
-//export function toSteps(dataTree: DataTree, expression: Expression, component: Component): Step[] {
-//  const fields = dataTree.expressionToFields(expression, component)
-//  return fields
-//    .map((type, index) => {
-//      const token = dataTree.fromStored(expression[index])
-//      if (!type) {
-//        console.error('Unknown type (reading type)', token)
-//        throw new Error(`Unknown type`)
-//      }
-//      const prev = index > 0 ? fields[index - 1] : null
-//      return toStep(dataTree, type, prev, token, component)
-//    })
-//}
-//
-///**
-// * Render an expression with the steps-selector web component
-// */
-//export function renderExpression(component: Component, dataTree: DataTree, stateId: StateId, label: string, allowFixed: boolean, reference: Ref<StepsSelector>, exposed: boolean, inputType?: FixedType, maxSteps?: number) {
-//  const stepsSelector = reference?.value
-//  const state = getState(component, stateId, exposed) ?? {expression: []}
-//  const steps = toSteps(
-//    dataTree,
-//    state.expression,
-//    component
-//  )
-//  const fixed = allowFixed && !state.expression.length || state.expression.length === 1 && steps[0].type === 'fixed'
-//  if(stepsSelector) {
-//    // This will not happen for the first render
-//    // The first render will use onload
-//    stepsSelector.steps = steps
-//    setCompletion(dataTree, component, stepsSelector)
-//  }
-//
-//  return html`
-//    <steps-selector
-//      ${ref(reference)}
-//      name=${stateId}
-//      group-by-category
-//      ?allow-fixed=${allowFixed}
-//      max-steps=${maxSteps ?? -1}
-//      input-type=${inputType}
-//      @load=${(e: CustomEvent) => {
-//        const stepsSelector = e.target as StepsSelector
-//        stepsSelector.steps = steps
-//        setCompletion(dataTree, component, stepsSelector)
-//      }}
-//      @change=${(e: SubmitEvent) => chagedStepsSelector(component, stateId, label, e.target as StepsSelector, exposed)}
-//      .fixed=${fixed}
-//      >
-//      ${label}
-//    </steps-selector>
-//  `
-//}
-//
-///**
-// * Render an option of a filter with the steps-selector web component
-// * Takes an expression of tokens and calls onChange with the updated expression
-// */
-//export function renderOption(opts: {
-//    component: Component,
-//    dataTree: DataTree,
-//    expression: Expression,
-//    name: string,
-//    label: string,
-//    allowFixed: boolean,
-//    reference: Ref<StepsSelector>,
-//    maxSteps?: number,
-//    inputType?: FixedType,
-//}): TemplateResult {
-//  const { component, dataTree, expression, label, name, allowFixed, reference, maxSteps, inputType } = opts
-//  const steps = toSteps(
-//    dataTree,
-//    expression,
-//    component
-//  )
-//  const fixed = allowFixed && expression.length || expression.length === 1 && steps[0].type === 'fixed'
-//  const stepsSelector = reference?.value
-//  if(stepsSelector) {
-//    // This will not happen for the first render
-//    // The first render will use onload
-//    stepsSelector.steps = steps
-//    setCompletion(dataTree, component, stepsSelector)
-//  }
-//  const value = JSON.stringify(expression)
-//  return html`
-//    <steps-selector
-//      value=${value}
-//      name=${name}
-//      input-type=${inputType}
-//      ${ref(reference)}
-//      group-by-category
-//      ?allow-fixed=${allowFixed}
-//      max-steps=${maxSteps ?? -1}
-//      @load=${(e: CustomEvent) => {
-//        const stepsSelector = e.target as StepsSelector
-//        stepsSelector.steps = steps
-//        setCompletion(dataTree, component, stepsSelector)
-//      }}
-//      @change=${(e: SubmitEvent) => {
-//        const stepsSelector = e.target as StepsSelector
-//        stepsSelector.steps = enrichSteps(stepsSelector.steps)
-//          .map(step => {
-//            const token = step.meta?.token
-//            if(!token) throw new Error('Token not found')
-//            return {
-//              ...step,
-//              meta: {
-//                ...step.meta,
-//                token,
-//              }
-//            }
-//          })
-//      }}
-//      .fixed=${fixed}
-//      >
-//      ${label}
-//    </steps-selector>
-//  `
-//}
-//
-///**
-// * Convert a token to a step
-// * This will resolve the types of filters
-// * @param field can be null, this happens when token is a filter with output resolving to null
-// */
-//export function toStep(dataTree: DataTree, field: Field | null, prev: Field | null, token: Token, component: Component): Step {
-//  switch (token.type) {
-//    case 'property':
-//      if(!field) {
-//        console.error('Unknown type (reading type)', token)
-//        throw new Error(`Unknown type`)
-//      }
-//      return {
-//        name: token.label,
-//        id: token.label + '_' + token.typeIds.join('_'),
-//        icon: '',
-//        type: getDisplayType(token.typeIds, token.kind),
-//        meta: { token, type: field },
-//        options: token.options,
-//        optionsForm: token.optionsForm ? addStyles(token.optionsForm(prev, token.options ?? {})) ?? null : null,
-//        category: token.dataSourceId as string | undefined,
-//      }
-//    case 'filter':
-//      return {
-//        name: token.label,
-//        id: token.id,
-//        icon: '',
-//        type: 'Filter',
-//        options: token.options,
-//        optionsForm: token.optionsForm ? addStyles(token.optionsForm(prev, token.options ?? {})) ?? null : null,
-//        meta: { token, type: field },
-//        category: 'Filters',
-//      }
-//    case 'state': {
-//      const parent = getParentByPersistentId(token.componentId, component)
-//      if(!parent) {
-//        console.warn('Component not found', token.componentId)
-//        // TODO: notification
-//      }
-//      return {
-//        name: getStateLabel(parent, token),
-//        id: token.componentId + '_' + token.storedStateId,
-//        icon: '',
-//        type: token.forceKind ?? field?.label ?? 'Unknown',
-//        meta: { token, type: field },
-//        category: 'States',
-//      }
-//    }
-//    default:
-//      console.error('Unknown token type (reading type)', token)
-//      throw new Error(`Unknown token type`)
-//  }
-//}
-
+/**
+ * Convert a token to a string
+ * This is used to store the token in the component
+ */
 export function toString(token: Token): string {
   return JSON.stringify({
     ...token,
   })
 }
 
+/**
+ * Convert a token to an option's tag value (json string)
+ */
 export function toId(token: Token): string {
   switch (token.type) {
     case 'property': return `property__${token.dataSourceId || ''}__${token.fieldId}`
@@ -366,14 +129,23 @@ export function toId(token: Token): string {
   }
 }
 
+/**
+ * Revert an option's tag value to a token
+ */
 export function fromString(editor: DataSourceEditor, id: string): Token {
   return editor.DataSourceManager.getDataTree().fromStored(JSON.parse(id)) as Token
 }
 
+/**
+ * Compare two tokens by stringifying them
+ */
 export function equals(a: Token, b: Token): boolean {
   return toId(a) === toId(b)
 }
 
+/**
+ * Apply a kind to a field
+ */
 export function convertKind(field: Field | null, from: FieldKind, to: FieldKind): Field | null {
   if (!field) {
     return null
@@ -387,6 +159,10 @@ export function convertKind(field: Field | null, from: FieldKind, to: FieldKind)
     kind: to,
   }
 }
+
+/**
+ * Get the type of a field, as provided by the data source
+ */
 export function getFieldType(editor: DataSourceEditor, field: Field | null, key: string | undefined): Field | null {
   const dataTree = editor.DataSourceManager.getDataTree()
   if (!field || !key) return null
@@ -406,6 +182,10 @@ export function getFieldType(editor: DataSourceEditor, field: Field | null, key:
     }
   }
 }
+
+/**
+ * Generate a form to edit the options of a token
+ */
 export function optionsFormKeySelector(editor: DataSourceEditor, field: Field | null, options: Options, name: string): TemplateResult {
   const dataTree = editor.DataSourceManager.getDataTree()
   if (!field) return html`
@@ -424,46 +204,3 @@ export function optionsFormKeySelector(editor: DataSourceEditor, field: Field | 
       </select>
     `
 }
-//export function optionsFormStateSelector(editor: DataSourceEditor, options: Options, name: string, reference: Ref<StepsSelector>, label: string = name): TemplateResult {
-//  const dataTree = editor.DataSourceManager.getDataTree()
-//  const component = editor.getSelected()
-//  if(!component) throw new Error('No component selected')
-//  // Fixme: Render the options form with the steps selector
-//  //const expression = JSON.parse(options[name] as string || '[]') as Expression
-//  //return renderOption({
-//  //  component,
-//  //  dataTree,
-//  //  expression,
-//  //  label,
-//  //  name,
-//  //  allowFixed: true,
-//  //  reference,
-//  //})
-//
-//  return html`
-//          <select name="${name}">
-//            <option value="">Select a custom state (${label})</option>
-//            ${
-//              dataTree.getContext()
-//      .filter(token => token.type === 'state' && token.exposed)
-//      .map(token => {
-//        const state = token as State
-//        const value = getStateVariableName(state.componentId, state.storedStateId)
-//        const component = (() => {
-//          let c = editor.getSelected()
-//          while (c) {
-//            if (getPersistantId(c) === state.componentId) return c
-//            c = c.parent()
-//          }
-//          return null
-//        })()
-//        if (!component) {
-//          console.warn(`Could not find component with persistent ID ${state.componentId}`, { state })
-//          return ''
-//        }
-//        return html`<option ${options[name] === value ? ' selected' : ''} value="${value}">${getStateLabel(component, state)}</option>`
-//      })
-//    }
-//          </select>
-//          `
-//}
