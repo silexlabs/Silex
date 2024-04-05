4aeb381267a84c3f99c6b47e50a72a7d43769dc7
Alex Hoyau <a.hoyau@silexlabs.org>
2024-04-05 18:56:20 +0500

    support alternative dropin replacements of google fonts

--- packages/grapesjs-fonts/src/fonts.js
+++ packages/grapesjs-fonts/src/fonts.js
@@ -24,7 +24,7 @@ let defaults = []
 /**
  * Options
  */
-let fontServer = 'https://fonts.googleapis.com'
+let fontServer = 'https://www.googleapis.com'
 
 /**
  * Load available fonts only once per session
