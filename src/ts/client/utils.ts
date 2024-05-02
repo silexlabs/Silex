09af50ab46ef8c5bf74b3eb4e0cd001319740bc2
oliviermgx <73127173+oliviermgx@users.noreply.github.com>
2024-05-02 14:25:35 +0200

    Update utils.ts
    commit readability

--- src/ts/client/utils.ts
+++ src/ts/client/utils.ts
@@ -26,38 +26,25 @@ export function onAll(editor: Editor, cbk: (c: Component) => void) {
         .onAll(c => cbk(c))
     })
 }
-/**
- * SHA256 hash a string
- 
-export async function hashString(str: string): Promise<string> {
-  // Convert the string to an ArrayBuffer
-  const encoder = new TextEncoder()
-  const data = encoder.encode(str)
-
-  // Hash the data with SHA-256
-  const hashBuffer = await crypto.subtle.digest('SHA-256', data)
-
-  // Convert the ArrayBuffer to hex string
-  const hashArray = Array.from(new Uint8Array(hashBuffer))
-  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
-
-  return hashHex
-}*/
 
 /**
  * SHA256 hash a string
  */
-export async function hashString(str: string): Promise<string> {
-  // Convert the string to an ArrayBuffer
-  const encoder = new TextEncoder()
-  const data = encoder.encode(str)
 
-  // Hash the data with SHA-256
+export async function hashString(str: string): Promise<string> {
   if (crypto.subtle != undefined) {
+    // Convert the string to an ArrayBuffer
+    const encoder = new TextEncoder()
+    const data = encoder.encode(str)
+
+    // Hash the data with SHA-256
     const hashBuffer = await crypto.subtle.digest('SHA-256', data)
+
     // Convert the ArrayBuffer to hex string
     const hashArray = Array.from(new Uint8Array(hashBuffer))
     const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
-    return hashHex}
+
+    return hashHex
+    }
   else {return 'local'}
 }
