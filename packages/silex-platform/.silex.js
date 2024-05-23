f24979d44e042f462b1aaf25f9406c15591d01a3
Alex Hoyau <a.hoyau@silexlabs.org>
2024-05-23 16:15:51 +0500

    new env vars for connectors

--- packages/silex-platform/.silex.js
+++ packages/silex-platform/.silex.js
@@ -1,59 +1,180 @@
 const { ConnectorType } = require('@silexlabs/silex/dist/server/types')
 const FtpConnector = require('@silexlabs/silex/dist/plugins/server/plugins/server/FtpConnector').default
-const DownloadPlugin = require('@silexlabs/silex/dist/plugins/server/plugins/server/DownloadConnector').default
+const DownloadConnector = require('@silexlabs/silex/dist/plugins/server/plugins/server/DownloadConnector').default
 const GitlabConnector = require('@silexlabs/silex/dist/plugins/server/plugins/server/GitlabConnector').default
+const GitlabHostingConnector = require('@silexlabs/silex/dist/plugins/server/plugins/server/GitlabHostingConnector').default
+const FsStorage = require('@silexlabs/silex/dist/server/server/connectors/FsStorage').default
 const dash = require('@silexlabs/silex-dashboard')
 const StaticPlugin = require('@silexlabs/silex/dist/plugins/server/plugins/server/StaticPlugin').default
 const node_modules = require('node_modules-path')
 const onboarding = require(__dirname + '/server-plugins/onboarding.js')
-
-// Create an alternate version of the the Gitlab connector
-class FramagitConnector extends GitlabConnector {
-  connectorId = 'framagit'
-  displayName = 'Framagit'
-  constructor(config, options) {
-    super(config, options)
-  }
-}
+const { join } = require('path')
 
 module.exports = async function (config) {
   await config.addPlugin(dash)
   await config.addPlugin(onboarding)
 
-  config.setHostingConnectors([
-    new FtpConnector(config, {
-      type: ConnectorType.HOSTING,
-    }),
-    new DownloadPlugin(config),
-  ])
+  initConnectors(config)
+  //config.setHostingConnectors([
+  //  new FtpConnector(config, {
+  //    type: ConnectorType.HOSTING,
+  //  }),
+  //  new DownloadPlugin(config),
+  //])
 
-  config.setStorageConnectors([
-    new FtpConnector(config, {
-      type: ConnectorType.STORAGE,
-    }),
-    new GitlabConnector(config, {
-      clientId: process.env.GITLAB_CLIENT_ID,
-      clientSecret: process.env.GITLAB_CLIENT_SECRET,
-      domain: process.env.GITLAB_DOMAIN,
-    }),
-    new FramagitConnector(config, {
-      clientId: process.env.FRAMAGIT_CLIENT_ID,
-      clientSecret: process.env.FRAMAGIT_CLIENT_SECRET,
-      domain: process.env.FRAMAGIT_DOMAIN,
-    }),
-  ])
+  //config.setStorageConnectors([
+  //  new FtpConnector(config, {
+  //    type: ConnectorType.STORAGE,
+  //  }),
+  //  new GitlabConnector(config, {
+  //    clientId: process.env.GITLAB_CLIENT_ID,
+  //    clientSecret: process.env.GITLAB_CLIENT_SECRET,
+  //    domain: process.env.GITLAB_DOMAIN,
+  //  }),
+  //  new FramagitConnector(config, {
+  //    clientId: process.env.FRAMAGIT_CLIENT_ID,
+  //    clientSecret: process.env.FRAMAGIT_CLIENT_SECRET,
+  //    domain: process.env.FRAMAGIT_DOMAIN,
+  //  }),
+  //])
 
   // CMS Plugin
   config.addPlugin(StaticPlugin, {
     routes: [
-      {
-        route: '/js/silex-cms/',
-        path: node_modules('@silexlabs/silex-cms') + '/@silexlabs/silex-cms/dist/',
-      },
       {
         route: '/js/client-plugins/',
         path: './client-plugins/',
       },
     ],
   })
+}
+
+const env = {
+  STORAGE_CONNECTORS: process.env.STORAGE_CONNECTORS || 'ftp,gitlab,gitlab2',
+  HOSTING_CONNECTORS: process.env.HOSTING_CONNECTORS || 'ftp,gitlab,gitlab2,download',
+  SILEX_FS_ROOT: process.env.SILEX_FS_ROOT || join(process.cwd(), '/silex/storage'),
+  SILEX_FS_HOSTING_ROOT: process.env.SILEX_FS_HOSTING_ROOT || join(process.cwd(), '/silex/hosting'),
+  GITLAB_DISPLAY_NAME: process.env.GITLAB_DISPLAY_NAME || 'Gitlab',
+  GITLAB_CLIENT_ID: process.env.GITLAB_CLIENT_ID,
+  GITLAB_CLIENT_SECRET: process.env.GITLAB_CLIENT_SECRET,
+  GITLAB_DOMAIN: process.env.GITLAB_DOMAIN,
+  GITLAB2_DISPLAY_NAME: process.env.GITLAB2_DISPLAY_NAME || 'Gitlab2',
+  GITLAB2_CLIENT_ID: process.env.GITLAB2_CLIENT_ID,
+  GITLAB2_CLIENT_SECRET: process.env.GITLAB2_CLIENT_SECRET,
+  GITLAB2_DOMAIN: process.env.GITLAB2_DOMAIN,
+  FTP_STORAGE_PATH: process.env.FTP_STORAGE_PATH || '/silex/storage',
+  FTP_HOSTING_PATH: process.env.FTP_HOSTING_PATH || '/silex/hosting',
+}
+
+// Create alternate versions of the the Gitlab connector
+class GitlabConnector1 extends GitlabConnector {
+  displayName = env.GITLAB_DISPLAY_NAME
+  constructor(config, options) {
+    super(config, options)
+  }
+}
+
+class GitlabConnector2 extends GitlabConnector {
+  connectorId = 'gitlab2'
+  displayName = env.GITLAB2_DISPLAY_NAME
+  constructor(config, options) {
+    super(config, options)
+  }
+}
+
+class GitlabHostingConnector1 extends GitlabHostingConnector {
+  displayName = env.GITLAB_DISPLAY_NAME
+  constructor(config, options) {
+    super(config, options)
+  }
+}
+
+class GitlabHostingConnector2 extends GitlabHostingConnector {
+  connectorId = 'gitlab2'
+  displayName = env.GITLAB2_DISPLAY_NAME
+  constructor(config, options) {
+    super(config, options)
+  }
+}
+
+function initConnectors(config) {
+  // Add storage and hosting connectors from env vars
+  if (env.STORAGE_CONNECTORS) {
+    config.setStorageConnectors([])
+    const connectors = env.STORAGE_CONNECTORS.split(',')
+    connectors.forEach((connector) => {
+      console.info('> Add storage connector from env var:', connector)
+      switch (connector) {
+        case 'fs':
+          config.addStorageConnector(new FsStorage(config, {
+            path: env.SILEX_FS_ROOT,
+          }))
+          break
+        case 'gitlab':
+          config.addStorageConnector(new GitlabConnector1(config, {
+            clientId: env.GITLAB_CLIENT_ID,
+            clientSecret: env.GITLAB_CLIENT_SECRET,
+            domain: env.GITLAB_DOMAIN,
+          }))
+          break
+        case 'gitlab2':
+          config.addStorageConnector(new GitlabConnector2(config, {
+            clientId: env.GITLAB2_CLIENT_ID,
+            clientSecret: env.GITLAB2_CLIENT_SECRET,
+            domain: env.GITLAB2_DOMAIN,
+          }))
+          break
+        case 'ftp':
+          config.addStorageConnector(new FtpConnector(config, {
+            type: ConnectorType.STORAGE,
+            path: env.FTP_STORAGE_PATH,
+          }))
+          break
+        default:
+          console.error('Unknown storage connector', connector)
+          throw new Error(`Unknown storage connector ${connector}`)
+      }
+    })
+  }
+
+  if (env.HOSTING_CONNECTORS) {
+    config.setHostingConnectors([])
+    const connectors = env.HOSTING_CONNECTORS.split(',')
+    connectors.forEach((connector) => {
+      console.info('> Add hosting connector from env var:', connector)
+      switch (connector) {
+        case 'fs':
+          config.addHostingConnector(new FsHosting(config, {
+            path: env.SILEX_FS_HOSTING_ROOT,
+          }))
+          break
+        case 'gitlab':
+          config.addHostingConnector(new GitlabHostingConnector1(config, {
+            clientId: env.GITLAB_CLIENT_ID,
+            clientSecret: env.GITLAB_CLIENT_SECRET,
+            domain: env.GITLAB_DOMAIN,
+          }))
+          break
+        case 'gitlab2':
+          config.addHostingConnector(new GitlabHostingConnector2(config, {
+            clientId: env.GITLAB2_CLIENT_ID,
+            clientSecret: env.GITLAB2_CLIENT_SECRET,
+            domain: env.GITLAB2_DOMAIN,
+          }))
+          break
+        case 'ftp':
+          config.addHostingConnector(new FtpConnector(config, {
+            type: ConnectorType.HOSTING,
+            path: env.FTP_HOSTING_PATH,
+          }))
+          break
+        case 'download':
+          config.addHostingConnector(new DownloadConnector(config))
+          break
+        default:
+          console.error('Unknown hosting connector', connector)
+          throw new Error(`Unknown hosting connector ${connector}`)
+      }
+    })
+  }
 }
--- packages/silex-platform/README.md
+++ packages/silex-platform/README.md
@@ -1,18 +1,39 @@
 ## Silex instances by Silex Labs
 
-This repo holds the code for the [public Silex instance hosted for free by Silex Labs foundation](https://editor.silex.me).
+This repo holds the code for the [public Silex instance hosted for free by Silex Labs foundation](https://editor.silex.me) and [The v3 instance too](https://v3.silex.me).
 
-This is also a good example on how to customize Silex.
+This is also a good example on how to customize Silex. And it has a Dockerfile for easy deployment
 
 ## Features
 
 This code adds features to the editor specific to our instance (in `index.js` and `index.pug`):
 
-* analytics - we use [microanalytics.io](https://microanalytics.io/) because it is green.
-* debugging with Sentry
-* a popin window which asks the user to rate the service on trustpilot
-* enable or disable cloud services and hosting providers
-* automatic deployment to heroku (see `.github/workflows/heroku.yml`)
+* [x] Multi-site with the Dashboard plugin
+* [x] Automatic deployment to [CapRover](https://caprover.com/) (see the captain-definition file and the file `.github/workflows/caprover.yml`)
+* [x] Onboarding: Send an email with [brevo the 1st time we see a user](https://brevo.co/) + use Silex notification system to guide users through the first steps
+* [x] Enable or disable cloud services and hosting providers with env vars
+* [ ] Analytics: add a tag in Silex editor
+
+## Environment variables
+
+You can set the following environment variables to customize the instance:
+
+| Name | Description | Type | Default value |
+|------|-------------| ---- |---------------|
+| `STORAGE_CONNECTORS` | List of storage connectors to enable | `ftp` or `gitlab` or `gitlab2` or `fs` | `ftp,gitlab,gitlab2` |
+| `HOSTING_CONNECTORS` | List of hosting connectors to enable | `ftp` or `gitlab` or `gitlab2` or `fs` or `download` | `ftp,gitlab,gitlab2,download` |
+| `SILEX_FS_ROOT` | Root folder for the file system storage | string | current directory + `/silex/storage/` |
+| `SILEX_FS_HOSTING_ROOT` | Root folder for the file system hosting | string | current directory + `/silex/hosting/` |
+| `FTP_STORAGE_PATH` | Path to the FTP storage | string | `/silex/storage/` |
+| `FTP_HOSTING_PATH` | Path to the FTP hosting | string | `/silex/hosting/` |
+| `GITLAB_DISPLAY_NAME` | Display name for the Gitlab storage | string | `Gitlab` |
+| `GITLAB_DOMAIN` | Domain of the Gitlab server, e.g `https://gitlab.com` | string | required with gitlab connector |
+| `GITLAB_CLIENT_ID` | Client ID for the Gitlab OAuth | string | required with gitlab connector |
+| `GITLAB_CLIENT_SECRET` | Client secret for the Gitlab OAuth | string | required with gitlab connector |
+| `GITLAB2_DISPLAY_NAME` | Display name for the 2nd Gitlab storage | string | `Gitlab2` |
+| `GITLAB2_DOMAIN` | Domain of the 2nd Gitlab server, e.g `https://gitlab.com` | string | required |
+| `GITLAB2_CLIENT_ID` | Client ID for the 2nd Gitlab OAuth | string | required |
+| `GITLAB2_CLIENT_SECRET` | Client secret for the 2nd Gitlab OAuth | string | required |
 
 ## Support
 
