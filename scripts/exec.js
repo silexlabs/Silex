32d84386c39dba469daef69d0fe4cbf45bb42039
Alex Hoyau <a.hoyau@silexlabs.org>
2024-05-23 18:14:13 +0500

    better formatting in exec script

--- scripts/exec.js
+++ scripts/exec.js
@@ -30,11 +30,21 @@ fs.readdir(directory, (err, files) => {
     if (fs.statSync(fullPath).isDirectory()) {
       try {
         // Display a separator and message
-        console.log('_'.repeat(100), `\ncd ${fullPath} && ${command}\n\n`);
+        // Display `file` in a box
+        console.log(`┌${'─'.repeat(file.length + 2)}┐`);
+        console.log(`│ ${file} │ `);
+        console.log(`└${'─'.repeat(file.length + 2)}┘`);
+        // Display in grey
+        console.log('\x1b[90m%s\x1b[0m', `Executing command \`${command}\``);
+
         // Change directory and execute the command
         process.chdir(fullPath);
         const result = spawnSync(command, { shell: true, stdio: 'inherit' });
-        console.log(result.status === 0 ? 'Success' : 'Error');
+        console.log('\n');
+        if(result.status !== 0) {
+          // Red error
+          console.error('\x1b[31m%s\x1b[0m', `Error executing command:`, result.error, '\n\n');
+        }
       } catch (error) {
         console.error(`Error executing command in ${fullPath}:`, error);
       }
