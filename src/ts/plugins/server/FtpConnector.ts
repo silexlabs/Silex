406be6d13fa6fc3cb02f203d13c13991e31ae0c5
John Doe <johndoe@example.com>
2024-09-24 16:54:45 +0000

    modif ftp connect2

--- src/ts/plugins/server/FtpConnector.ts
+++ src/ts/plugins/server/FtpConnector.ts
@@ -416,7 +416,6 @@ export default class FtpConnector implements StorageConnector<FtpSession> {
       // Return all meta
       return {
         websiteId,
-        //url: await this.getFileUrl(session, websiteId, WEBSITE_DATA_FILE_NAME),
         ...meta,
       }
     } catch(err) {
