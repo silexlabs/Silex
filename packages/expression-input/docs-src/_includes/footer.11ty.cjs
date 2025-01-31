5d4430416157356d661725368370703192da4d10
Alex Hoyau <a.hoyau@silexlabs.org>
2025-01-31 23:38:16 +0800

    chore: remove eleventy site

--- packages/expression-input/docs-src/_includes/footer.11ty.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = function (data) {
-  return `
-<footer>
-  <p>
-    Made with
-    <a href="https://github.com/lit/lit-element-starter-ts">lit-starter-ts</a>
-  </p>
-</footer>`;
-};
