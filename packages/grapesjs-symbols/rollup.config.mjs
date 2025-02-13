7b9bd155918b70c101939eda356a55e25f5b2504
Alex Hoyau <a.hoyau@silexlabs.org>
2025-02-13 15:45:36 +0800

    bundle for direct links in browsers

---
+++ packages/grapesjs-symbols/rollup.config.mjs
@@ -0,0 +1,39 @@
+import summary from 'rollup-plugin-summary'
+import terser from '@rollup/plugin-terser'
+import resolve from '@rollup/plugin-node-resolve'
+
+export default {
+  input: 'dist/es5/index.js', // Single entry point that imports all dependencies
+  output: [
+    {
+      file: './dist/bundle.iife.js', // IIFE for browsers
+      format: 'iife',
+      name: 'ExpressionInputBundle',
+      sourcemap: true,
+    },
+    {
+      file: './dist/bundle.esm.js', // ESM for frontend frameworks
+      format: 'esm',
+      sourcemap: true,
+    }
+  ],
+  onwarn(warning) {
+    if (warning.code !== 'THIS_IS_UNDEFINED') {
+      console.error(`(!) ${warning.message}`)
+    }
+  },
+  plugins: [
+    resolve({ browser: true }),
+    terser({
+      ecma: 2017,
+      module: true,
+      warnings: true,
+      mangle: {
+        properties: {
+          regex: /^__/,
+        },
+      },
+    }),
+    summary(),
+  ],
+}
