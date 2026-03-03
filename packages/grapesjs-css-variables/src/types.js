5b04d285a3122dd4a93b07e08894f64c6637bee8
Alex Hoyau <a.hoyau@silexlabs.org>
2026-03-03 16:14:22 +0100

    chore: missed a file in last commit

---
+++ packages/grapesjs-css-variables/src/types.js
@@ -0,0 +1,8 @@
+/**
+ * Variable type constants
+ */
+export const TYPE_COLOR = 'color'
+export const TYPE_SIZE = 'size'
+export const TYPE_FONT_FAMILY = 'font-family'
+
+export const VARIABLE_TYPES = [TYPE_COLOR, TYPE_SIZE, TYPE_FONT_FAMILY]
