57fec3690cdbe761ce5878f1ea8776b58ce0d60a
Alex Hoyau <a.hoyau@silexlabs.org>
2026-03-29 00:58:58 +0100

    chore(ux): rename "components" to "elements" in user-facing strings

--- packages/grapesjs-notifications/src/locale/en.js
+++ packages/grapesjs-notifications/src/locale/en.js
@@ -2,7 +2,7 @@ export default {
     '@silexlabs/grapesjs-notifications': {
         'Close': '\u2716 Dismiss',
         'CloseAll': '\u2716 Dismiss all',
-        'Select': '\u2713 Select component',
+        'Select': '\u2713 Select element',
         'Show': '\u2193 Show',
     },
 };
